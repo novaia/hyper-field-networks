a7b928a942d476019688093778eae83589744375
hayden-donnelly <hayden.git@proton.me>
2023-09-15 17:32:13 -0400

    Added neck and head generation.

--- scripts/generate_chubby.py
+++ scripts/generate_chubby.py
@@ -112,6 +112,63 @@ ops_mesh.extrude_vertices_move(
 )
 
 b_mesh.verts.ensure_lookup_table()
+b_mesh.verts[4].select = False
+b_mesh.verts[5].select = False
+b_mesh.verts[1].select = True
+
+# Create neck base by extruding up.
+ops_mesh.extrude_vertices_move(
+    MESH_OT_extrude_verts_indiv={'mirror':False}, 
+    TRANSFORM_OT_translate={
+        'value':(0, 0, 0.5), 
+        'orient_type':'GLOBAL', 
+        'orient_matrix':((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+        'orient_matrix_type':'GLOBAL', 
+        'constraint_axis':(False, False, True), 
+        'mirror':True
+    }
+)
+
+# Create neck by extruding up.
+ops_mesh.extrude_vertices_move(
+    MESH_OT_extrude_verts_indiv={'mirror':False}, 
+    TRANSFORM_OT_translate={
+        'value':(0, 0, 1), 
+        'orient_type':'GLOBAL', 
+        'orient_matrix':((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+        'orient_matrix_type':'GLOBAL', 
+        'constraint_axis':(False, False, True), 
+        'mirror':True
+    }
+)
+
+# Create head base by extruding forward.
+ops_mesh.extrude_vertices_move(
+    MESH_OT_extrude_verts_indiv={'mirror':False}, 
+    TRANSFORM_OT_translate={
+        'value':(0, 0.6, 0), 
+        'orient_type':'GLOBAL', 
+        'orient_matrix':((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+        'orient_matrix_type':'GLOBAL', 
+        'constraint_axis':(False, True, False), 
+        'mirror':True
+    }
+)
+
+# Create head top by extruding upwards.
+ops_mesh.extrude_vertices_move(
+    MESH_OT_extrude_verts_indiv={'mirror':False}, 
+    TRANSFORM_OT_translate={
+        'value':(0, 0, 1), 
+        'orient_type':'GLOBAL', 
+        'orient_matrix':((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+        'orient_matrix_type':'GLOBAL', 
+        'constraint_axis':(False, False, True), 
+        'mirror':True
+    }
+)
+
+b_mesh.verts.ensure_lookup_table()
 
 spine_radius = random.uniform(0.4, 1)
 spine_radius = (spine_radius, spine_radius)
@@ -121,6 +178,12 @@ hip_radius = random.uniform(0.4, 1)
 hip_radius = (hip_radius, hip_radius)
 collar_bone_radius = random.uniform(0.4, 0.6)
 collar_bone_radius = (collar_bone_radius, collar_bone_radius)
+neck_radius = random.uniform(0.2, 0.6)
+neck_radius = (neck_radius, neck_radius)
+head_base_radius = random.uniform(0.5, 2)
+head_base_radius = (head_base_radius, head_base_radius)
+head_top_radius = random.uniform(0.5, 2)
+head_top_radius = (head_top_radius, head_top_radius)
 
 # TODO: mark vertex 0 as root for skin modifier so symmetry is maintained,
 # when using different scales for each axis.
@@ -137,5 +200,11 @@ b_mesh.verts[0][skin_layer].radius = root_radius
 b_mesh.verts[3][skin_layer].radius = hip_radius
 # Collar bone.
 b_mesh.verts[4][skin_layer].radius = collar_bone_radius
+# Neck.
+b_mesh.verts[7][skin_layer].radius = neck_radius
+# Head base.
+b_mesh.verts[8][skin_layer].radius = head_base_radius
+# Head top.
+b_mesh.verts[9][skin_layer].radius = head_top_radius
 
 bpy.ops.wm.save_as_mainfile(filepath='data/blend_files/test.blend')

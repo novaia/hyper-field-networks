3f31b07dff25064150a35f10d6b8bb9aca714245
hayden-donnelly <hayden.git@proton.me>
2023-10-07 14:59:17 -0400

    Cleaned up ingp introspection.

--- scripts/read_ingp.py
+++ scripts/read_ingp.py
@@ -1,13 +1,9 @@
-# Import the modules
 import gzip
 import msgpack
 
-# Define the file name
 file_name = "data/base.ingp"
 
-# Open the file in binary mode
 with gzip.open(file_name, "rb") as f:
-    # Read the file content
     file_content = f.read()
     file_content = msgpack.unpackb(file_content)
     print(file_content.keys())
@@ -17,9 +13,3 @@ with gzip.open(file_name, "rb") as f:
     print(file_content['snapshot']['n_params'])
     params = msgpack.unpackb(file_content['snapshot']['params_binary'])
     print(params)
-    
-    #file_content = gzip.decompress(file_content)
-    
-    # Unpack the file content as a msgpack object
-    # Print the file content
-    #print(file_content)

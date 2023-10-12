12058bc32f03178f18ea6ec0b1a9904215e6fb6e
hayden-donnelly <hayden.git@proton.me>
2023-10-12 19:18:18 -0400

    Moved instant nerf config to top of if __main__.

--- scripts/instant_nerf.py
+++ scripts/instant_nerf.py
@@ -889,18 +889,39 @@ def load_lego_dataset(path:str, downscale_factor:int, translation_scale:float):
 if __name__ == '__main__':
     print('GPU:', jax.devices('gpu'))
 
+    num_hash_table_levels = 16
+    max_hash_table_entries = 2**20
+    hash_table_feature_dim = 2
+    coarsest_resolution = 16
+    finest_resolution = 2**19
+    density_mlp_width = 64
+    color_mlp_width = 64
+    high_dynamic_range = False
+    exponential_density_activation = False
+
+    learning_rate = 1e-2
+    epsilon = 1e-15
+    weight_decay_coefficient = 1e-6
+    ray_near = 0.2
+    ray_far = 3.0
+    batch_size = 10000
+    num_ray_samples = 32
+    training_steps = 400
+    num_turntable_render_frames = 10
+    turntable_render_camera_distance = 2.0
+    render_patch_size_x = 32
+    render_patch_size_y = 32
+
+    assert ray_near < ray_far, 'Ray near must be less than ray far.'
+
     #dataset = load_lego_dataset('data/lego', 0.33)
-    dataset = load_lego_dataset('data/lego', 8, 0.33)
+    dataset = load_lego_dataset('data/lego', 2, 0.33)
     #dataset = load_lego_dataset('data/lego', 2, 0.33)
     #dataset = load_dataset('data/generation_0', 2, 0.1)
     print(dataset.horizontal_fov)
     print(dataset.vertical_fov)
     print(dataset.fl_x)
     print(dataset.fl_y)
-    print(dataset.k1)
-    print(dataset.k2)
-    print(dataset.p1)
-    print(dataset.p2)
     print(dataset.cx)
     print(dataset.cy)
     print(dataset.w)
@@ -909,44 +930,33 @@ if __name__ == '__main__':
     print('Images shape:', dataset.images.shape)
 
     model = InstantNerf(
-        number_of_grid_levels=16,
-        max_hash_table_entries=2**20,
-        hash_table_feature_dim=2,
-        coarsest_resolution=16,
-        finest_resolution=2**19,
-        density_mlp_width=64,
-        color_mlp_width=64,
-        high_dynamic_range=False,
-        exponential_density_activation=False
+        number_of_grid_levels=num_hash_table_levels,
+        max_hash_table_entries=max_hash_table_entries,
+        hash_table_feature_dim=hash_table_feature_dim,
+        coarsest_resolution=coarsest_resolution,
+        finest_resolution=finest_resolution,
+        density_mlp_width=density_mlp_width,
+        color_mlp_width=color_mlp_width,
+        high_dynamic_range=high_dynamic_range,
+        exponential_density_activation=exponential_density_activation
     )
     rng = jax.random.PRNGKey(1)
     state = create_train_state(
-        model=model, 
-        rng=rng, 
-        learning_rate=1e-2, 
-        epsilon=1e-15, 
-        weight_decay_coefficient=1e-6
+        model=model, rng=rng, learning_rate=learning_rate, 
+        epsilon=epsilon, weight_decay_coefficient=weight_decay_coefficient
     )
-
-    ray_near = 0.2
-    ray_far = 3.0
-    assert ray_near < ray_far, 'Ray near must be less than ray far.'
-
+    del rng
     state = train_loop(
-        batch_size=4096,
-        num_ray_samples=32,
-        ray_near=ray_near,
-        ray_far=ray_far,
-        training_steps=400, 
-        state=state, 
-        dataset=dataset
+        batch_size=batch_size, num_ray_samples=num_ray_samples, ray_near=ray_near, 
+        ray_far=ray_far, training_steps=training_steps, state=state, dataset=dataset
     )
+
     turntable_render(
-        num_frames=10, 
-        num_ray_samples=64, 
-        patch_size_x=32, 
-        patch_size_y=32, 
-        camera_distance=1.0, 
+        num_frames=num_turntable_render_frames, 
+        num_ray_samples=num_ray_samples, 
+        patch_size_x=render_patch_size_x, 
+        patch_size_y=render_patch_size_y, 
+        camera_distance=turntable_render_camera_distance, 
         ray_near=ray_near, 
         ray_far=ray_far, 
         state=state, 
@@ -955,9 +965,9 @@ if __name__ == '__main__':
     )
     generate_density_grid(32, 32, 32, state)
     render_scene(
-        num_ray_samples=32, 
-        patch_size_x=32, 
-        patch_size_y=32, 
+        num_ray_samples=num_ray_samples, 
+        patch_size_x=render_patch_size_x, 
+        patch_size_y=render_patch_size_y, 
         ray_near=ray_near, 
         ray_far=ray_far, 
         dataset=dataset, 
@@ -966,9 +976,9 @@ if __name__ == '__main__':
         file_name='instant_rendered_image_0'
     )
     render_scene(
-        num_ray_samples=32, 
-        patch_size_x=32, 
-        patch_size_y=32, 
+        num_ray_samples=num_ray_samples, 
+        patch_size_x=render_patch_size_x, 
+        patch_size_y=render_patch_size_y, 
         ray_near=ray_near, 
         ray_far=ray_far, 
         dataset=dataset, 
@@ -977,9 +987,9 @@ if __name__ == '__main__':
         file_name='instant_rendered_image_1'
     )
     render_scene(
-        num_ray_samples=32, 
-        patch_size_x=32, 
-        patch_size_y=32, 
+        num_ray_samples=num_ray_samples, 
+        patch_size_x=render_patch_size_x, 
+        patch_size_y=render_patch_size_y, 
         ray_near=ray_near, 
         ray_far=ray_far, 
         dataset=dataset, 

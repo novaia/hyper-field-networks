e76085d6e024515bb63dac8bbb9cd1ad11c96d2e
hayden-donnelly <hayden.git@proton.me>
2023-10-15 14:41:03 -0400

    Added arg to preview packed shape without training.

--- scripts/generate_image_fields.py
+++ scripts/generate_image_fields.py
@@ -9,6 +9,48 @@ import json
 from fields import image_field
 import argparse
 
+def pack_weights(state, packed_width):
+    packed_height = 0
+    weight_map = []
+    packed_weights = []
+    for key in state.params.keys():
+        new_weight_map_entry = {'layer': key}
+        for sub_key in state.params[key].keys():
+            parameter = state.params[key][sub_key]
+            parameter_shape = parameter.shape
+            if sub_key == 'bias':
+                new_weight_map_entry['bias_width'] = parameter_shape[0]
+                packed_weights.append(jnp.expand_dims(
+                    jnp.concatenate([
+                        parameter, jnp.zeros((packed_width - parameter_shape[0]))
+                    ], axis=-1),
+                    axis=0
+                ))
+                packed_height += 1
+            elif sub_key == 'kernel':
+                height_greater_than_width = parameter_shape[0] > parameter_shape[1]
+                height_equal_to_packed_width = parameter_shape[0] == packed_width
+                # The second operand is important because we don't want to transpose
+                # unless it will create a shape that can be concatenated with the other
+                # parameters along the height axis, i.e. (parameter_height, packed_width).
+                if height_greater_than_width and height_equal_to_packed_width:
+                    kernel_height = parameter_shape[1]
+                    kernel_width = parameter_shape[0]
+                    packed_weights.append(jnp.transpose(parameter))
+                    new_weight_map_entry['kernel_transposed'] = True
+                    packed_height += kernel_height
+                else:
+                    kernel_height = parameter_shape[0]
+                    kernel_width = parameter_shape[1]
+                    packed_weights.append(parameter)
+                    new_weight_map_entry['kernel_transposed'] = False
+                    packed_height += kernel_height
+                new_weight_map_entry['kernel_height'] = kernel_height
+                new_weight_map_entry['kernel_width'] = kernel_width
+        weight_map.append(new_weight_map_entry)
+    packed_weights = jnp.concatenate(packed_weights, axis=0)
+    return packed_weights, weight_map
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_generations', type=int, default=21551)
@@ -19,7 +61,17 @@ if __name__ == '__main__':
     parser.add_argument('--encoding_dim', type=int, default=10)
     parser.add_argument('--image_directory', type=str, default='data/anime_faces')
     parser.add_argument('--save_directory', type=str, default='data/approximation_field')
+    parser.add_argument('--preview_packed_shape', type=bool, default=False)
     args = parser.parse_args()
+
+    if args.preview_packed_shape:
+        model = image_field.ImageField(
+            mlp_depth=args.mlp_depth, mlp_width=args.mlp_width, encoding_dim=10
+        )
+        state = image_field.create_train_state(model, 1e-3, jax.random.PRNGKey(0))
+        packed_weights, weight_map = pack_weights(state, args.mlp_width)
+        print('Packed weights shape:', packed_weights.shape)
+        exit(0)
 
     image_paths = os.listdir(args.image_directory)
     for i in range(args.num_generations):
@@ -36,47 +88,8 @@ if __name__ == '__main__':
             state, image.shape[0], image.shape[1], 
             os.path.join(args.save_directory, f'{i}_image.png')
         )
-        packed_height = 0
-        packed_width = args.mlp_width
-        weight_map = []
-        packed_weights = []
-        for key in state.params.keys():
-            new_weight_map_entry = {'layer': key}
-            for sub_key in state.params[key].keys():
-                parameter = state.params[key][sub_key]
-                parameter_shape = parameter.shape
-                if sub_key == 'bias':
-                    new_weight_map_entry['bias_width'] = parameter_shape[0]
-                    packed_weights.append(jnp.expand_dims(
-                        jnp.concatenate([
-                            parameter, jnp.zeros((packed_width - parameter_shape[0]))
-                        ], axis=-1),
-                        axis=0
-                    ))
-                    packed_height += 1
-                elif sub_key == 'kernel':
-                    height_greater_than_width = parameter_shape[0] > parameter_shape[1]
-                    height_equal_to_packed_width = parameter_shape[0] == packed_width
-                    # The second operand is important because we don't want to transpose
-                    # unless it will create a shape that can be concatenated with the other
-                    # parameters along the height axis, i.e. (parameter_height, packed_width).
-                    if height_greater_than_width and height_equal_to_packed_width:
-                        kernel_height = parameter_shape[1]
-                        kernel_width = parameter_shape[0]
-                        packed_weights.append(jnp.transpose(parameter))
-                        new_weight_map_entry['kernel_transposed'] = True
-                        packed_height += kernel_height
-                    else:
-                        kernel_height = parameter_shape[0]
-                        kernel_width = parameter_shape[1]
-                        packed_weights.append(parameter)
-                        new_weight_map_entry['kernel_transposed'] = False
-                        packed_height += kernel_height
-                    new_weight_map_entry['kernel_height'] = kernel_height
-                    new_weight_map_entry['kernel_width'] = kernel_width
-            weight_map.append(new_weight_map_entry)
 
-        packed_weights = jnp.concatenate(packed_weights, axis=0)
+        packed_weights, weight_map = pack_weights(state, args.mlp_width)
         packed_weights_image = packed_weights - jnp.min(packed_weights)
         packed_weights_image = packed_weights_image / jnp.max(packed_weights_image)
         plt.imsave(

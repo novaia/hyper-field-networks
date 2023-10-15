501845fae10ba8cb79f6f9459b45cbc66e1b75a3
hayden-donnelly <hayden.git@proton.me>
2023-10-15 14:31:25 -0400

    Added check to ensure weight transpose will produce compatible shape.
    Also added cli args.

--- scripts/generate_image_fields.py
+++ scripts/generate_image_fields.py
@@ -7,29 +7,37 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import json
 from fields import image_field
+import argparse
 
 if __name__ == '__main__':
-    num_generations = 2000
-    train_steps = 1000
-    batch_size = 32
-    mlp_width = 64
-    image_directory = 'data/anime_faces'
-    image_paths = os.listdir(image_directory)
-    save_directory = 'data/approximation_field'
-    for i in range(num_generations):
-        image = Image.open(os.path.join(image_directory, image_paths[i]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_generations', type=int, default=21551)
+    parser.add_argument('--train_steps', type=int, default=1000)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--mlp_width', type=int, default=64)
+    parser.add_argument('--mlp_depth', type=int, default=3)
+    parser.add_argument('--encoding_dim', type=int, default=10)
+    parser.add_argument('--image_directory', type=str, default='data/anime_faces')
+    parser.add_argument('--save_directory', type=str, default='data/approximation_field')
+    args = parser.parse_args()
+
+    image_paths = os.listdir(args.image_directory)
+    for i in range(args.num_generations):
+        image = Image.open(os.path.join(args.image_directory, image_paths[i]))
         image = jnp.array(image) / 255.0
-        model = image_field.ImageField(mlp_depth=3, mlp_width=mlp_width, encoding_dim=10)
+        model = image_field.ImageField(
+            mlp_depth=args.mlp_depth, mlp_width=args.mlp_width, encoding_dim=10
+        )
         state = image_field.create_train_state(model, 1e-3, jax.random.PRNGKey(0))
         state = image_field.train_loop(
-            image, image.shape[0], image.shape[1], batch_size, train_steps, state
+            image, image.shape[0], image.shape[1], args.batch_size, args.train_steps, state
         )
         image_field.draw_image(
             state, image.shape[0], image.shape[1], 
-            os.path.join(save_directory, f'{i}_image.png')
+            os.path.join(args.save_directory, f'{i}_image.png')
         )
         packed_height = 0
-        packed_width = mlp_width
+        packed_width = args.mlp_width
         weight_map = []
         packed_weights = []
         for key in state.params.keys():
@@ -47,7 +55,12 @@ if __name__ == '__main__':
                     ))
                     packed_height += 1
                 elif sub_key == 'kernel':
-                    if parameter_shape[0] > parameter_shape[1]:
+                    height_greater_than_width = parameter_shape[0] > parameter_shape[1]
+                    height_equal_to_packed_width = parameter_shape[0] == packed_width
+                    # The second operand is important because we don't want to transpose
+                    # unless it will create a shape that can be concatenated with the other
+                    # parameters along the height axis, i.e. (parameter_height, packed_width).
+                    if height_greater_than_width and height_equal_to_packed_width:
                         kernel_height = parameter_shape[1]
                         kernel_width = parameter_shape[0]
                         packed_weights.append(jnp.transpose(parameter))
@@ -62,16 +75,16 @@ if __name__ == '__main__':
                     new_weight_map_entry['kernel_height'] = kernel_height
                     new_weight_map_entry['kernel_width'] = kernel_width
             weight_map.append(new_weight_map_entry)
-        
+
         packed_weights = jnp.concatenate(packed_weights, axis=0)
         packed_weights_image = packed_weights - jnp.min(packed_weights)
         packed_weights_image = packed_weights_image / jnp.max(packed_weights_image)
         plt.imsave(
-            os.path.join(save_directory, f'{i}_weights.png'), 
+            os.path.join(args.save_directory, f'{i}_weights.png'), 
             packed_weights_image, cmap='magma'
         )
-        jnp.save(os.path.join(save_directory, f'{i}_weights.npy'), packed_weights)
+        jnp.save(os.path.join(args.save_directory, f'{i}_weights.npy'), packed_weights)
 
-    with open(os.path.join(save_directory, f'{i}_weight_map.json'), 'w') as f:
+    with open(os.path.join(args.save_directory, f'weight_map.json'), 'w') as f:
         json.dump(weight_map, f, indent=4)
     

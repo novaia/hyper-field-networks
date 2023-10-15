8bd66e2fad717ff8e97a2322242f9b266026f57b
hayden-donnelly <hayden.git@proton.me>
2023-10-15 16:56:55 -0400

    Added cli args to unpack_field_weights.

--- scripts/unpack_field_weights.py
+++ scripts/unpack_field_weights.py
@@ -7,17 +7,28 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import json
 from fields import image_field
+import argparse
 
 if __name__ == '__main__':
-    weight_path = 'data/approximation_field/0_weights.npy'
-    weight_map_path = 'data/approximation_field/weight_map.json'
-    weight_map = json.load(open(weight_map_path, 'r'))
-    weights = jnp.load(weight_path)
-    
-    model = image_field.ImageField(mlp_depth=3, mlp_width=64, encoding_dim=10)
+    parser = argparse.ArgumentParser()
+    default_weight_path = 'data/generated_weights/1_weights.npy'
+    default_weight_map_path = 'data/approximation_field_small/weight_map.json'
+    parser.add_argument('--weight_path', type=str, default=default_weight_path)
+    parser.add_argument('--weight_map_path', type=str, default=default_weight_map_path)
+    parser.add_argument('--mlp_width', type=int, default=64)
+    parser.add_argument('--mlp_depth', type=int, default=3)
+    parser.add_argument('--encoding_dim', type=int, default=10)
+    args = parser.parse_args()
+
+    weight_map = json.load(open(args.weight_map_path, 'r'))
+    weights = jnp.load(args.weight_path)
+    model = image_field.ImageField(
+        mlp_depth=args.mlp_depth, mlp_width=args.mlp_width, encoding_dim=args.encoding_dim
+    )
     state = image_field.create_train_state(model, 1e-3, jax.random.PRNGKey(0))
-    
+    print('Packed weights shape:', weights.shape)
     print(weight_map)
+
     current_height = 0
     for i in range(len(weight_map)):
         layer_map = weight_map[i]

4034bdd3ccbfb0fd060d7d35369a4cd7f23ad26d
hayden-donnelly <hayden.git@proton.me>
2023-10-16 16:51:46 -0400

    Added cli args to turntable_video.

--- scripts/turntable_video.py
+++ scripts/turntable_video.py
@@ -1,24 +1,28 @@
 import cv2
+import argparse
 
-frame_path = 'data/instant_turntable_render_frame_'
-num_frames = 60 * 3
-width = 800
-height = 800
-depth = False
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--frame_path', type=str, default='data/instant_turntable_render_frame_')
+    parser.add_argument('--num_frames', type=int, default=60*3)
+    parser.add_argument('--width', type=int, default=512)
+    parser.add_argument('--height', type=int, default=512)
+    parser.add_argument('--depth', type=bool, default=False)
+    args = parser.parse_args()
 
-video = cv2.VideoWriter(
-    'data/instant_turntable_video.mp4', 
-    cv2.VideoWriter_fourcc(*"mp4v"), 
-    60, 
-    (width, height)
-)
+    video = cv2.VideoWriter(
+        'data/instant_turntable_video.mp4', 
+        cv2.VideoWriter_fourcc(*"mp4v"), 
+        60, 
+        (args.width, args.height)
+    )
 
-for i in range(num_frames):
-    if depth:
-        current_frame_path = frame_path + str(i) + '_depth' + '.png'
-    else:
-        current_frame_path = frame_path + str(i) + '.png'
-    image = cv2.imread(current_frame_path)
-    video.write(image)
+    for i in range(args.num_frames):
+        if args.depth:
+            current_frame_path = args.frame_path + str(i) + '_depth' + '.png'
+        else:
+            current_frame_path = args.frame_path + str(i) + '.png'
+        image = cv2.imread(current_frame_path)
+        video.write(image)
 
-video.release()
+    video.release()

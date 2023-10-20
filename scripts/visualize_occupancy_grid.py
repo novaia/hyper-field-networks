e577197ac7dac9f39ffafef068dd2ceb8a215e4c
hayden-donnelly <hayden.git@proton.me>
2023-10-20 17:32:49 -0400

    Decreased point size and added optional gui.
    Currently the gui cannot be viewed from inside the docker container, so
    you will have to run the script on your host machine if you use the
    --gui True argument.

--- scripts/visualize_occupancy_grid.py
+++ scripts/visualize_occupancy_grid.py
@@ -1,31 +1,45 @@
 from simple_3dviz.behaviours.misc import LightToCamera
-from simple_3dviz.behaviours.movements import RotateModel
 from simple_3dviz.behaviours.io import SaveFrames
 from simple_3dviz import Spherecloud
 from simple_3dviz.utils import render
-import jax.numpy as jnp
+import numpy as np
+import argparse
 
-density_threshold = 6.0
-occupancy_grid_density = jnp.load('data/occupancy_grid_density.npy')
-occupancy_grid_coordinates = jnp.load('data/occupancy_grid_coordinates.npy')
+parser = argparse.ArgumentParser()
+parser.add_argument('--density_threshold', type=float, default=6.0)
+parser.add_argument('--gui', type=bool, default=False)
+args = parser.parse_args()
 
-thresholded_indices = jnp.where(occupancy_grid_density > density_threshold)
+occupancy_grid_density = np.load('data/occupancy_grid_density.npy')
+occupancy_grid_coordinates = np.load('data/occupancy_grid_coordinates.npy')
+
+num_coordinates = occupancy_grid_coordinates.shape[0]
+num_densities = occupancy_grid_density.shape[0]
+assert num_coordinates == num_densities, 'Number of coordinates and densities must match, ' + \
+    f'got {num_coordinates} and {num_densities}'
+
+
+thresholded_indices = np.where(occupancy_grid_density > args.density_threshold)
 thresholded_coordinates = occupancy_grid_coordinates[thresholded_indices]
+assert thresholded_coordinates.shape[0] > 0, 'There were no densities above the threshold.'
 
 point_cloud = Spherecloud(
     centers=thresholded_coordinates,
     colors=(0.5, 0.5, 0.5),
-    sizes=0.5
+    sizes=0.01
 )
 
-#show(mesh, size=(800, 600), behaviours=[RotateModel(), LightToCamera()])
-image = render(
-    point_cloud,
-    behaviours=[
-        RotateModel(), 
-        LightToCamera(), 
-        SaveFrames("data/occupancy_render/frame_{:03d}.png", every_n=1)
-    ],
-    n_frames=1,
-    light=(-60., -160., 120)
-)
+if args.gui:
+    from simple_3dviz.window import show
+    show(point_cloud, size=(800, 800), behaviours=[LightToCamera()])
+else:
+    image = render(
+        point_cloud,
+        behaviours=[
+            LightToCamera(), 
+            SaveFrames("data/occupancy_render/frame_{:03d}.png", every_n=1)
+        ],
+        n_frames=1,
+        camera_position=(-1, -1, -1),
+        light=(-60., -160., 120)
+    )

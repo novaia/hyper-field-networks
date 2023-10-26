de213992f27fbf9134617551b245a6071e826c4b
hayden-donnelly <hayden.git@proton.me>
2023-10-25 20:48:31 -0400

    Stripped encoding.cu down to just grid encoding.

--- dependencies/tiny-cuda-nn/src/encoding.cu
+++ dependencies/tiny-cuda-nn/src/encoding.cu
@@ -29,14 +29,8 @@
 
 #include <tiny-cuda-nn/encoding.h>
 
-#include <tiny-cuda-nn/encodings/composite.h>
-#include <tiny-cuda-nn/encodings/empty.h>
-#include <tiny-cuda-nn/encodings/frequency.h>
 #include <tiny-cuda-nn/encodings/grid.h>
-#include <tiny-cuda-nn/encodings/identity.h>
-#include <tiny-cuda-nn/encodings/oneblob.h>
-#include <tiny-cuda-nn/encodings/spherical_harmonics.h>
-#include <tiny-cuda-nn/encodings/triangle_wave.h>
+//#include <tiny-cuda-nn/encodings/spherical_harmonics.h>
 
 
 namespace tcnn {
@@ -54,6 +48,7 @@ template <typename T>
 auto register_builtin_encodings() {
 	ci_hashmap<std::function<Encoding<T>*(uint32_t, const json&)>> factories;
 
+	/*
 	register_encoding<T>(factories, "Composite", [](uint32_t n_dims_to_encode, const json& encoding) {
 		return new CompositeEncoding<T>{encoding, n_dims_to_encode};
 	});
@@ -65,6 +60,7 @@ auto register_builtin_encodings() {
 	register_encoding<T>(factories, "Frequency", [](uint32_t n_dims_to_encode, const json& encoding) {
 		return new FrequencyEncoding<T>{encoding.value("n_frequencies", 12u), n_dims_to_encode};
 	});
+	*/
 
 	auto grid_factory = [](uint32_t n_dims_to_encode, const json& encoding) {
 		return create_grid_encoding<T>(n_dims_to_encode, encoding);
@@ -74,6 +70,7 @@ auto register_builtin_encodings() {
 	register_encoding<T>(factories, "TiledGrid", grid_factory);
 	register_encoding<T>(factories, "DenseGrid", grid_factory);
 
+	/*
 	register_encoding<T>(factories, "Identity", [](uint32_t n_dims_to_encode, const json& encoding) {
 		return new IdentityEncoding<T>{n_dims_to_encode, encoding.value("scale", 1.0f), encoding.value("offset", 0.0f)};
 	});
@@ -89,7 +86,9 @@ auto register_builtin_encodings() {
 	register_encoding<T>(factories, "TriangleWave", [](uint32_t n_dims_to_encode, const json& encoding) {
 		return new TriangleWaveEncoding<T>{encoding.value("n_frequencies", 12u), n_dims_to_encode};
 	});
+	*/
 
+	/*
 	auto nrc_factory = [](uint32_t n_dims_to_encode, const json& encoding) {
 		return new CompositeEncoding<T>{
 			{
@@ -113,6 +112,7 @@ auto register_builtin_encodings() {
 	};
 	register_encoding<T>(factories, "OneBlobFrequency", nrc_factory);
 	register_encoding<T>(factories, "NRC", nrc_factory);
+	*/
 
 	return factories;
 }

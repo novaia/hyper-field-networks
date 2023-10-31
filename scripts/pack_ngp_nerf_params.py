4ff33713cc86f38310b3ca1ebd00c44dd493401c
hayden-donnelly <hayden.git@proton.me>
2023-10-30 21:26:23 -0400

    Added ability to pack directory of param files.

--- scripts/pack_ngp_nerf_params.py
+++ scripts/pack_ngp_nerf_params.py
@@ -1,6 +1,9 @@
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+import argparse
+import json
+import os
 
 def pack_weights(params, packed_width):
     packed_height = 0
@@ -84,25 +87,48 @@ def unpack_weights(packed_weights, weight_map):
     print(current_height)
     return unpacked_weights
 
+def get_file_paths_in_subdirs(dir):
+    if os.path.isdir(dir):
+        files = []
+        for subdir in os.listdir(dir):
+            subdir_path = os.path.join(dir, subdir)
+            if os.path.isdir(subdir_path):
+                files.extend(get_file_paths_in_subdirs(subdir_path))
+            else:
+                files.append(subdir_path)
+        return files
+    raise ValueError('The specified directory does not exist')
+
 def main():
-    loss_threshold = 1e-3
-    params = jnp.load('data/synthetic_nerfs/aliens/0-39/alien_10.npy', allow_pickle=True)
-    params = params.tolist()
-    final_loss = params['final_loss']
-    if final_loss > loss_threshold:
-        print('Loss is too high, skipping')
-        return
-    params = params['params']
-    packed_weights, weight_map = pack_weights(params, 64)
-    print('Packed weights shape:', packed_weights.shape)
-    print('Weight map:', weight_map)
-    unpacked_weights = unpack_weights(packed_weights, weight_map)
-    print(params['MultiResolutionHashEncoding_0']['hash_table'].shape)
-    print(unpacked_weights['MultiResolutionHashEncoding_0']['hash_table'].shape)
-    print('Correct unpack', jnp.allclose(
-        params['MultiResolutionHashEncoding_0']['hash_table'],
-        unpacked_weights['MultiResolutionHashEncoding_0']['hash_table']
-    ))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str)
+    parser.add_argument('--output_dir', type=str)
+    parser.add_argument('--unpack', type=bool, default=False)
+    parser.add_argument(
+        '--rm', type=bool, default=False, help='Remove original files after packing'
+    )
+    parser.add_argument('--loss_threshold', type=float, default=1e-3)
+    args = parser.parse_args()
+    assert args.input_dir is not None, 'Must specify a directory'
+    assert args.unpack == False, 'Unpacking is not yet implemented'
+
+    file_paths = get_file_paths_in_subdirs(args.input_dir)
+    for path in file_paths:
+        params = jnp.load(path, allow_pickle=True).tolist()
+        print(f'Loaded {path}')
+        if params['final_loss'] > args.loss_threshold:
+            print('Loss is too high, skipping')
+            continue
+        params = params['params']
+        packed_params, param_map = pack_weights(params, 64)
+        save_path = os.path.join(args.output_dir, os.path.basename(path))
+        jnp.save(save_path, packed_params)
+        print(f'Saved to {save_path}')
+        if args.rm:
+            os.remove(path)
+            print(f'Removed {path}')
+    with open(os.path.join(args.output_dir, 'param_map.json'), 'w+') as f:
+        json.dump(param_map, f, indent=4)
 
 if __name__ == '__main__':
     main()

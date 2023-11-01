fc88678ce67b11116438264622773fc7192ce0b9
hayden-donnelly <hayden.git@proton.me>
2023-10-31 21:20:58 -0400

    Added script to train hypernets.

---
+++ scripts/train_hypernet.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+from hypernets import hyper_diffusion
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--net', type=str)
+    parser.add_argument('--dataset_path', type=str)
+    parser.add_argument('--downscale_factor', type=int, default=1)
+    args = parser.parse_args()
+    if args.net == 'hyper_diffusion':
+        hyper_diffusion.main()
+    else:
+        raise ValueError(f'Unknown network: {args.net}')

5cb038edb00a2be24c038aaf8be8f08b539ba26f
hayden-donnelly <hayden.git@proton.me>
2023-11-01 02:00:09 -0400

    Working train loop for ngp hyper diffusion.

--- hypernets/ngp_hyper_diffusion.py
+++ hypernets/ngp_hyper_diffusion.py
@@ -12,6 +12,118 @@ from typing import List, Tuple
 from operator import itemgetter
 import math
 
+@dataclass
+class DatasetInfo:
+    file_paths: List[str]
+    num_files: int
+    num_tokens: int
+    token_dim: int
+    num_contexts: int
+    batch_size: int
+    context_length: int
+    files_to_load: int # Number of files needed to fill a batch.
+    samples_per_file: int # Number of samples to take from each file for batch.
+    loaded_shape: Tuple[int, int, int] # Shape of loaded batch files.
+    pad_shape: Tuple[int, int, int] # Shape of padding needed to split file into num_contexts.
+    final_shape: Tuple[int, int, int] # Shape of final batch.
+    context_indices: jax.Array
+    file_indices: jax.Array
+
+def create_dataset_info(path:str, context_length:int, batch_size:int, verbose:bool):
+    path_list = os.listdir(path)
+    valid_file_paths = []
+    for file_path in path_list:
+        if file_path.endswith('.npy'):
+            valid_file_paths.append(os.path.join(path, file_path))
+    num_files = len(valid_file_paths)
+    assert num_files > 0, 'No .npy files found in specified directory'
+    base_sample = jnp.load(valid_file_paths[0])
+    assert len(base_sample.shape) == 2, 'Samples arrays must be 2D'
+    num_tokens = base_sample.shape[0]
+    token_dim = base_sample.shape[1]
+    pad_size = (context_length - (num_tokens % context_length)) % context_length
+    num_contexts = (num_tokens + pad_size) // context_length
+    context_indices = jnp.arange(context_length)
+    file_indices = jnp.arange(num_files)
+    files_to_load = math.ceil(batch_size / num_contexts)
+    loaded_shape = (files_to_load, num_tokens, token_dim)
+    pad_shape = (files_to_load, pad_size, token_dim)
+    samples_per_file = batch_size // files_to_load
+    final_shape = (batch_size, context_length, token_dim)
+
+    if verbose:
+        print('Number of files:', num_files)
+        print('Number of tokens per sample:', num_tokens)
+        print('Token dim:', token_dim)
+        print('Context length:', context_length)
+        print('Number of contexts per sample:', num_contexts)
+        print('Loaded shape:', loaded_shape)
+        print('Pad shape:', pad_shape)
+        print('Files to load:', files_to_load)
+        print('Batch size:', batch_size)
+        print('Context length:', context_length)
+
+    dataset_info = DatasetInfo(
+        file_paths=valid_file_paths,
+        num_files=num_files,
+        num_tokens=num_tokens,
+        token_dim=token_dim,
+        loaded_shape=loaded_shape,
+        pad_shape=pad_shape,
+        final_shape=final_shape,
+        num_contexts=num_contexts,
+        files_to_load=files_to_load,
+        samples_per_file=samples_per_file,
+        batch_size=batch_size,
+        context_length=context_length,
+        context_indices=context_indices,
+        file_indices=file_indices,
+    )
+    return dataset_info
+
+def load_batch(dataset_info:DatasetInfo, key):
+    sample_key, context_key = jax.random.split(key, num=2)
+
+    # Load all files required for the batch.
+    file_indices = jax.random.choice(
+        sample_key, 
+        dataset_info.file_indices, 
+        shape=(dataset_info.files_to_load,), 
+        replace=False
+    )
+    batch_paths = itemgetter(*file_indices.tolist())(dataset_info.file_paths)
+    if type(batch_paths) == str:
+        batch_paths = [batch_paths]
+    else:
+        batch_paths = list(batch_paths)
+    batch = []
+    for path in batch_paths:
+        batch.append(jnp.load(path))
+    batch = jnp.array(batch, dtype=jnp.float32)
+    batch = jnp.reshape(batch, dataset_info.loaded_shape)
+
+    # Pad the batch.
+    batch = jnp.concatenate([jnp.zeros(dataset_info.pad_shape), batch], axis=1)
+
+    # Split the batch into multiple contexts.
+    split_contexts_shape = (
+        dataset_info.files_to_load,
+        dataset_info.num_contexts, 
+        dataset_info.context_length, 
+        dataset_info.token_dim
+    )
+    batch = jnp.reshape(batch, split_contexts_shape)
+
+    # Select random contexts.
+    context_indices = jax.random.choice(
+        context_key,
+        dataset_info.context_indices,
+        shape=(dataset_info.samples_per_file,),
+        replace=True
+    )
+    batch = batch[:, context_indices, :, :]
+    batch = jnp.reshape(batch, dataset_info.final_shape)
+    return batch, context_indices
 
 def sinusoidal_embedding(x, embedding_max_frequency, embedding_dims):
     embedding_min_frequency = 1.0
@@ -41,15 +153,18 @@ class HyperDiffusion(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        x, noise_variances = x
-        embeded_noise_variances = sinusoidal_embedding(
+        x, context_indices, noise_variances = x
+        embedded_noise_variances = sinusoidal_embedding(
             noise_variances, self.embedding_max_frequency, self.token_dim
         )
-        x = jnp.concatenate([x, embeded_noise_variances], axis=-2)
+        embedded_context_indices = sinusoidal_embedding(
+            context_indices, self.embedding_max_frequency, self.token_dim
+        )
+        x = jnp.concatenate([x, embedded_noise_variances, embedded_context_indices], axis=-2)
         x = nn.Dense(features=self.embedded_token_dim)(x)
-        positions = jnp.arange(self.context_length+1)
+        positions = jnp.arange(self.context_length+2)
         embedded_position = nn.Embed(
-            num_embeddings=self.context_length+1, features=self.embedded_token_dim
+            num_embeddings=self.context_length+2, features=self.embedded_token_dim
         )(positions)
         x = x + embedded_position
         for _ in range(self.num_blocks):
@@ -67,7 +182,7 @@ class HyperDiffusion(nn.Module):
             x = nn.activation.relu(x)
             x = nn.LayerNorm()(x + residual)
         x = nn.Dense(features=self.token_dim)(x)
-        x = x[:, :-1, :] # Remove embedded noise variances token.
+        x = x[:, :-2, :] # Remove embedded noise variances and context indices tokens.
         return x
     
 def diffusion_schedule(diffusion_times, min_signal_rate, max_signal_rate):
@@ -81,46 +196,47 @@ def diffusion_schedule(diffusion_times, min_signal_rate, max_signal_rate):
     return noise_rates, signal_rates
 
 def create_train_state(model, rng, learning_rate, context_length, token_dim, steps_per_epoch):
-    x = (jnp.ones([1, context_length, token_dim]), jnp.ones([1, 1, 1]))
+    x = (jnp.ones([1, context_length, token_dim]), jnp.ones([1, 1, 1]), jnp.ones([1, 1, 1]))
     variables = model.init(rng, x)
     params = variables['params']
-    learning_rate_schedule = optax.exponential_decay(
-        learning_rate, transition_begin=80*steps_per_epoch, 
-        transition_steps=100*steps_per_epoch, decay_rate=0.8
-    )
-    tx = optax.adam(learning_rate_schedule)
+    tx = optax.adam(learning_rate)
     ts = TrainState.create(apply_fn=model.apply, params=params, tx=tx)
     return ts
 
-def train_loop(dataset, epochs, batch_size, min_signal_rate, max_signal_rate, state):
-    steps_per_epoch = dataset.shape[0] // batch_size
+def train_loop(
+    datset_info:DatasetInfo, epochs:int, min_signal_rate:float, max_signal_rate:float, 
+    state:TrainState
+):
+    steps_per_epoch = datset_info.num_files // datset_info.batch_size
     losses = []
     for epoch in range(epochs):
         for step in range(steps_per_epoch):
             losses_this_epoch = []
             step_key = jax.random.PRNGKey(epoch * steps_per_epoch + step)
-            batch_indices_key, step_key = jax.random.split(step_key, num=2)
-            batch_indices = jax.random.randint(
-                batch_indices_key, shape=(batch_size,), minval=0, maxval=dataset.shape[0]
-            )
-            batch = dataset[batch_indices]
+            batch_key, train_key = jax.random.split(step_key, 2)
+            batch, context_indices = load_batch(datset_info, batch_key)
             loss, state = train_step(
-                batch=batch, 
+                batch=batch,
+                context_indices=context_indices,
                 min_signal_rate=min_signal_rate,
                 max_signal_rate=max_signal_rate,
                 state=state,
-                parent_key=step_key
+                parent_key=train_key
             )
-            #print('Step', step, 'Loss:', loss)
+            print('Step', step, 'Loss:', loss)
             losses_this_epoch.append(loss)
         losses.append(sum(losses_this_epoch) / len(losses_this_epoch))
         print('Epoch', epoch, 'Loss:', losses[-1])
     return state
 
 @jax.jit
-def train_step(batch, min_signal_rate, max_signal_rate, state, parent_key):
+def train_step(
+    batch:jax.Array, context_indices:jax.Array, min_signal_rate:float, max_signal_rate:float, 
+    state:TrainState, parent_key
+):
     noise_key, diffusion_time_key = jax.random.split(parent_key, 2)
-    
+    context_indices = jnp.reshape(context_indices, (context_indices.shape[0], 1, 1))
+
     def loss_fn(params):
         noises = jax.random.normal(noise_key, batch.shape)
         diffusion_times = jax.random.uniform(diffusion_time_key, (batch.shape[0], 1, 1))
@@ -128,8 +244,8 @@ def train_step(batch, min_signal_rate, max_signal_rate, state, parent_key):
             diffusion_times, min_signal_rate, max_signal_rate
         )
         noisy_batch = signal_rates * batch + noise_rates * noises
-
-        pred_noises = state.apply_fn({'params': params}, [noisy_batch, noise_rates**2])
+        x = [noisy_batch, context_indices, noise_rates**2]
+        pred_noises = state.apply_fn({'params': params}, x)
 
         loss = jnp.mean((pred_noises - noises)**2)
         return loss
@@ -184,160 +300,30 @@ def reverse_diffusion(
 
     return pred_images
 
-def load_dataset(path:str, context_length:int):
-    dataset_directory_list = os.listdir(path)
-    dataset = []
-    for file in dataset_directory_list:
-        if file.endswith('.npy'):
-            dataset.append(jnp.load(os.path.join(path, file)))
-            break
-    dataset = jnp.array(dataset, dtype=jnp.float32)
-    pad_size = (context_length - (dataset.shape[1] % context_length)) % context_length
-    dataset = jnp.concatenate([
-        jnp.zeros((dataset.shape[0], pad_size, dataset.shape[2])),
-        dataset,
-    ], axis=1)
-    num_samples = dataset.shape[0]
-    num_contexts = dataset.shape[1] // context_length
-    token_dim = dataset.shape[2]
-    dataset = jnp.reshape(dataset, (num_samples, num_contexts, context_length, token_dim))
-    return dataset
-
-@dataclass
-class DatasetInfo:
-    sample_paths: List[str]
-    num_samples: int
-    num_tokens: int
-    token_dim: int
-    num_contexts: int
-    batch_size: int
-    context_length: int
-    files_to_load: int # Number of files needed to fill a batch.
-    samples_per_file: int # Number of samples to take from each file for batch.
-    loaded_shape: Tuple[int, int, int] # Shape of loaded batch files.
-    pad_shape: Tuple[int, int, int] # Shape of padding needed to split file into num_contexts.
-    final_shape: Tuple[int, int, int] # Shape of final batch.
-    context_indices: jax.Array
-    sample_indices: jax.Array
-
-def create_dataset_info(path:str, context_length:int, batch_size:int, verbose:bool):
-    path_list = os.listdir(path)
-    sample_paths = []
-    for file_path in path_list:
-        if file_path.endswith('.npy'):
-            sample_paths.append(os.path.join(path, file_path))
-    num_samples = len(sample_paths)
-    assert num_samples > 0, 'No .npy files found in specified directory'
-    base_sample = jnp.load(sample_paths[0])
-    assert len(base_sample.shape) == 2, 'Samples arrays must be 2D'
-    num_tokens = base_sample.shape[0]
-    token_dim = base_sample.shape[1]
-    pad_size = (context_length - (num_tokens % context_length)) % context_length
-    num_contexts = (num_tokens + pad_size) // context_length
-    context_indices = jnp.arange(context_length)
-    sample_indices = jnp.arange(num_samples)
-    files_to_load = math.ceil(batch_size / num_contexts)
-    loaded_shape = (files_to_load, num_tokens, token_dim)
-    pad_shape = (files_to_load, pad_size, token_dim)
-    samples_per_file = batch_size // files_to_load
-    final_shape = (batch_size, context_length, token_dim)
-
-    if verbose:
-        print('Number of samples:', num_samples)
-        print('Number of tokens per sample:', num_tokens)
-        print('Token dim:', token_dim)
-        print('Context length:', context_length)
-        print('Number of contexts per sample:', num_contexts)
-        print('Loaded shape:', loaded_shape)
-        print('Pad shape:', pad_shape)
-        print('Files to load:', files_to_load)
-        print('Batch size:', batch_size)
-        print('Context length:', context_length)
-
-    dataset_info = DatasetInfo(
-        sample_paths=sample_paths,
-        num_samples=num_samples,
-        num_tokens=num_tokens,
-        token_dim=token_dim,
-        loaded_shape=loaded_shape,
-        pad_shape=pad_shape,
-        final_shape=final_shape,
-        num_contexts=num_contexts,
-        files_to_load=files_to_load,
-        samples_per_file=samples_per_file,
-        batch_size=batch_size,
-        context_length=context_length,
-        context_indices=context_indices,
-        sample_indices=sample_indices,
-    )
-    return dataset_info
-
-def load_batch(dataset_info:DatasetInfo, key):
-    sample_key, context_key = jax.random.split(key, num=2)
-
-    # Load all files required for the batch.
-    sample_indices = jax.random.choice(
-        sample_key, 
-        dataset_info.num_samples, 
-        shape=(dataset_info.files_to_load,), 
-        replace=False
-    )
-    batch_paths = itemgetter(*sample_indices.tolist())(dataset_info.sample_paths)
-    if type(batch_paths) == str:
-        batch_paths = [batch_paths]
-    else:
-        batch_paths = list(batch_paths)
-    batch = []
-    for path in batch_paths:
-        batch.append(jnp.load(path))
-    batch = jnp.array(batch, dtype=jnp.float32)
-    batch = jnp.reshape(batch, dataset_info.loaded_shape)
-
-    # Pad the batch.
-    batch = jnp.concatenate([jnp.zeros(dataset_info.pad_shape), batch], axis=1)
-
-    # Split the batch into multiple contexts.
-    split_contexts_shape = (
-        dataset_info.files_to_load,
-        dataset_info.num_contexts, 
-        dataset_info.context_length, 
-        dataset_info.token_dim
-    )
-    batch = jnp.reshape(batch, split_contexts_shape)
-
-    # Select random contexts.
-    context_indices = jax.random.choice(
-        context_key,
-        dataset_info.context_indices,
-        shape=(dataset_info.samples_per_file,),
-        replace=True
-    )
-    batch = batch[:, context_indices, :, :]
-    batch = jnp.reshape(batch, dataset_info.final_shape)
-    return batch
-
 def main():
     print('GPU:', jax.devices('gpu'))
 
     epochs = 1000
-    batch_size = 32
+    batch_size = 8
     min_signal_rate = 0.02
     max_signal_rate = 0.95
     embedding_max_frequency = 1000.0
-    num_blocks = 1
+    num_blocks = 4
     feed_forward_dim = 512
     attention_dim = 512
     embedded_token_dim = 512
     attention_heads = 8
     learning_rate = 5e-5
-    context_length = 2048
+    context_length = 1024
 
-    datast_info = create_dataset_info(
+    dataset_info = create_dataset_info(
         'data/synthetic_nerfs/packed_aliens', context_length, batch_size, verbose=True
     )
-    batch = load_batch(datast_info, jax.random.PRNGKey(0))
+    batch, context_indices = load_batch(dataset_info, jax.random.PRNGKey(0))
     print(batch.shape)
-    exit(0)
+    print(context_indices.shape)
+    token_dim = batch.shape[-1]
+    steps_per_epoch = (dataset_info.num_files * dataset_info.num_tokens) // batch_size
 
     model = HyperDiffusion(
         num_blocks=num_blocks,
@@ -354,7 +340,14 @@ def main():
         model, rng, learning_rate, context_length, token_dim, steps_per_epoch
     )
     del rng
-    state = train_loop(dataset, epochs, batch_size, min_signal_rate, max_signal_rate, state)
+    state = train_loop(
+        dataset_info, 
+        epochs,
+        min_signal_rate, 
+        max_signal_rate, 
+        state
+    )
+    exit(0)
     generated_weights = reverse_diffusion(
         apply_fn=state.apply_fn, 
         params=state.params, 
@@ -367,7 +360,6 @@ def main():
         max_signal_rate=max_signal_rate,
         seed=0
     )
-    exit(0)
     generated_weights = jnp.squeeze(generated_weights)
     generated_weights = jnp.reshape(generated_weights, (generated_weights.shape[0], 48, 16))
     for i in range(generated_weights.shape[0]):

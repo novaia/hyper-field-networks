7ec1841346031518ec7d2c567e6485d1c51a87b6
hayden-donnelly <hayden.git@proton.me>
2023-11-05 01:31:02 -0400

    Recompute attention during backward pass.

--- hypernets/sparse_transformer.py
+++ hypernets/sparse_transformer.py
@@ -5,21 +5,15 @@ from flax.training.train_state import TrainState
 import optax
 import matplotlib.pyplot as plt
 
-def sinusoidal_embedding(x, embedding_max_frequency, embedding_dims):
-    embedding_min_frequency = 1.0
-    frequencies = jnp.exp(
-        jnp.linspace(
-            jnp.log(embedding_min_frequency),
-            jnp.log(embedding_max_frequency),
-            embedding_dims // 2
-        )
-    )
-    angular_speeds = 2.0 * jnp.pi * frequencies
-    embeddings = jnp.concatenate(
-        [jnp.sin(angular_speeds * x), jnp.cos(angular_speeds * x)],
-        axis = -1
-    )
-    return embeddings
+def diffusion_schedule(diffusion_times, min_signal_rate, max_signal_rate):
+    start_angle = jnp.arccos(max_signal_rate)
+    end_angle = jnp.arccos(min_signal_rate)
+
+    diffusion_angles = start_angle + diffusion_times * (end_angle - start_angle)
+
+    signal_rates = jnp.cos(diffusion_angles)
+    noise_rates = jnp.sin(diffusion_angles)
+    return noise_rates, signal_rates
 
 class SparseTransformer(nn.Module):
     attention_dim:int
@@ -32,24 +26,52 @@ class SparseTransformer(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        @jax.custom_jvp
+        def attention_projection(x):
+            def project_and_expand(x):
+                return jnp.expand_dims(nn.Dense(features=self.attention_dim)(x), axis=-1)
+            query = project_and_expand(x)
+            key = project_and_expand(x)
+            value = project_and_expand(x)
+            return query, key, value
+
+        @jax.custom_vjp
+        def recomputed_attention(query, key, value):
+            return nn.dot_product_attention(query, key, value)
+        def recomputed_attention_fwd(query, key, value):
+            residuals = (query, key, value)
+            return recomputed_attention(query, key, value), residuals
+        def recomputed_attention_bwd(residuals, gradients):
+            query, key, value = residuals
+            _, vjp_fn = jax.vjp(nn.dot_product_attention, query, key, value)
+            return vjp_fn(gradients)
+        recomputed_attention.defvjp(recomputed_attention_fwd, recomputed_attention_bwd)
+
         def attention(x):
-            query = nn.Dense(features=self.attention_dim)(x)
-            key = nn.Dense(features=self.attention_dim)(x)
-            value = nn.Dense(features=self.attention_dim)(x)
-            x = nn.dot_product_attention(query, key, value)
+            query, key, value = attention_projection(x)
+            x = recomputed_attention(query, key, value)
+            x = jnp.sum(x, axis=-1)
             x = nn.Dense(features=self.embedding_dim)(x)
             x = nn.gelu(x)
             return x
-        attention_fwd = lambda x: attention(x)
-        attention_bwd = lambda x, g: jax.grad(attention)(x) * g
-        attention.defjvp(attention_fwd, attention_bwd)
+
+        def sinusoidal_embedding(x):
+            embedding_min_frequency = 1.0
+            frequencies = jnp.exp(
+                jnp.linspace(
+                    jnp.log(embedding_min_frequency),
+                    jnp.log(self.embedding_max_frequency),
+                    self.token_dim // 2
+                )
+            )
+            angular_speeds = 2.0 * jnp.pi * frequencies
+            embeddings = jnp.concatenate(
+                [jnp.sin(angular_speeds * x), jnp.cos(angular_speeds * x)],
+                axis = -1
+            )
+            return embeddings
 
         x, noise_variances = x
-        embeded_noise_variances = sinusoidal_embedding(
-            noise_variances, self.embedding_max_frequency, self.token_dim
-        )
-        print(embeded_noise_variances.shape)
+        embeded_noise_variances = sinusoidal_embedding(noise_variances)
         x = jnp.concatenate([x, embeded_noise_variances], axis=-2)
         x = nn.Dense(features=self.embedding_dim)(x)
         positions = jnp.arange(self.context_length+1)
@@ -71,15 +93,35 @@ class SparseTransformer(nn.Module):
         x = nn.Dense(features=self.token_dim)(x)
         return x
 
+def train_step(state:TrainState, key:int, batch:jax.Array):
+    noise_key, diffusion_time_key = jax.random.split(key)
+    diffusion_times = jax.random.uniform(diffusion_time_key, (batch.shape[0], 1, 1))
+    noise_rates, signal_rates = diffusion_schedule(diffusion_times, 0.01, 0.99)
+    noise = jax.random.normal(noise_key, batch.shape)
+    noisy_batch = batch * signal_rates + noise * noise_rates
+
+    def loss_fn(params):
+        x = (noisy_batch, noise_rates**2)
+        y = state.apply_fn({'params': params}, x)
+        return jnp.mean((y - batch)**2)
+    
+    grad_fn = jax.value_and_grad(loss_fn)
+    loss, grad = grad_fn(state.params)
+    state = state.apply_gradients(grads=grad)
+    return loss, state
+
+def save_image(image, file_path):
+    image = image - jnp.min(image)
+    image = image / jnp.max(image)
+    plt.imsave(file_path, image)
+
 def main():
     sequence_height = 300
     sequence_width = 40
-    sequence = jnp.arange(sequence_height) * jnp.pi / 8
+    sequence = jnp.arange(sequence_height) * jnp.pi / 16
     sequence = jnp.sin(sequence)
     sequence = jnp.repeat(jnp.expand_dims(sequence, axis=-1), sequence_width, axis=-1)
-    sequence = sequence - jnp.min(sequence)
-    sequence = sequence / jnp.max(sequence)
-    plt.imsave('data/sequence.png', sequence)
+    save_image(sequence, 'data/sequence.png')
     sequence = jnp.expand_dims(sequence, axis=0)
 
     model = SparseTransformer(
@@ -93,9 +135,17 @@ def main():
     )
     tx = optax.adam(1e-3)
     rng = jax.random.PRNGKey(0)
-    x = (jnp.ones(sequence.shape), jnp.ones((sequence.shape[0], 1, 1)))
+    x = (jnp.ones(sequence.shape), jnp.ones((1, 1, 1)))
     params = model.init(rng, x)['params']
     state = TrainState.create(apply_fn=model.apply, params=params, tx=tx)
+
+    batch_size = 1
+    train_steps = 100
+    for step in range(train_steps):
+        step_key = jax.random.PRNGKey(step)
+        batch = sequence
+        loss, state = train_step(state, step_key, batch)
+        print('loss:', loss)
 
 if __name__ == '__main__':
     main()

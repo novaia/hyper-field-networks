4e4545ddea2acb691a5b236b3aefd1d7fb0a6ec2
hayden-donnelly <hayden.git@proton.me>
2023-11-06 13:34:30 -0500

    Moved alpha compositing functions into common.rendering.

--- fields/__init__.py
+++ fields/__init__.py
@@ -1,10 +1,1 @@
-from typing import Optional
-from dataclasses import dataclass
-import jax.numpy as jnp
-import jax
-
-def straight_alpha_composite(foreground, background, alpha):
-    return foreground * alpha + background * (1 - alpha)
-
-def alpha_composite(foreground, background, alpha):
-    return foreground + background * (1 - alpha)
+# hi
---
+++ fields/common/rendering.py
@@ -0,0 +1,7 @@
+import jax
+
+def straight_alpha_composite(foreground:jax.Array, background:jax.Array, alpha:jax.Array):
+    return foreground * alpha + background * (1 - alpha)
+
+def alpha_composite(foreground:jax.Array, background:jax.Array, alpha:jax.Array):
+    return foreground + background * (1 - alpha)

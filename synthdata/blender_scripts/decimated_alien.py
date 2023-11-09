affa4b9b52a78146af75a47e8f6c98a4afd1c73a
hayden-donnelly <hayden.git@proton.me>
2023-11-09 16:55:18 -0500

    Changed synthdata import alias from sdh to sd.

--- synthdata/blender_scripts/decimated_alien.py
+++ synthdata/blender_scripts/decimated_alien.py
@@ -5,10 +5,10 @@ import os
 import sys
 print(os.getcwd())
 sys.path.append(os.getcwd())
-import synthdata as sdh
+import synthdata as sd
 
 def generate():
-    extrude = sdh.global_mirror_offset_extrude
+    extrude = sd.global_mirror_offset_extrude
     rng = random.uniform
     
     hip_offset = (rng(0.4, 1), rng(-0.2, 0.2), rng(-0.4, 0))
@@ -155,24 +155,24 @@ def add_sun():
     bpy.data.lights['Sun'].color = (1.0, 0.655, 0.101)
 
 def main():
-    args = sdh.get_arguments()
-    sdh.delete_all_and_add_camera()
+    args = sd.get_arguments()
+    sd.delete_all_and_add_camera()
     d_obj = generate()
-    sphere_radius = sdh.move_bounding_sphere_to_origin(d_obj)
+    sphere_radius = sd.move_bounding_sphere_to_origin(d_obj)
     add_sun()
-    sdh.set_renderer_cycles_gpu()
-    sdh.set_background((0.7, 0.7, 0.7), 0.5)
-    sdh.set_general_render_settings()
-    sdh.set_cycles_render_settings()
+    sd.set_renderer_cycles_gpu()
+    sd.set_background((0.7, 0.7, 0.7), 0.5)
+    sd.set_general_render_settings()
+    sd.set_cycles_render_settings()
     camera = bpy.context.scene.objects['Camera']
-    extrinsic_camera_data = sdh.random_render_on_sphere(
+    extrinsic_camera_data = sd.random_render_on_sphere(
         camera, bpy.context.scene, sphere_radius+10, 
         (0, 0, 0), args.num_renders, args.save_directory
     )
-    intrinsic_camera_data = sdh.get_intrinsic_camera_data()
-    transform_data = sdh.build_transform_data(intrinsic_camera_data, extrinsic_camera_data)
-    sdh.save_transform_data(transform_data, args.save_directory)
-    sdh.fetch_and_save_tri_count(args.save_directory)
+    intrinsic_camera_data = sd.get_intrinsic_camera_data()
+    transform_data = sd.build_transform_data(intrinsic_camera_data, extrinsic_camera_data)
+    sd.save_transform_data(transform_data, args.save_directory)
+    sd.fetch_and_save_tri_count(args.save_directory)
     bpy.ops.wm.save_as_mainfile(filepath='data/blend_files/test.blend')
 
 if __name__ == '__main__':
--- synthdata/blender_scripts/premade.py
+++ synthdata/blender_scripts/premade.py
@@ -3,28 +3,28 @@ import os
 import sys
 print(os.getcwd())
 sys.path.append(os.getcwd())
-import synthdata as sdh
+import synthdata as sd
 
 def main():
-    args = sdh.get_arguments()
+    args = sd.get_arguments()
     bpy.ops.wm.open_mainfile(filepath='data/blend_files/3d_anime_girl.blend')
     bpy.ops.object.camera_add()
     bpy.context.scene.camera = bpy.context.object
     d_obj = bpy.context.scene.objects['Body']
-    sphere_radius = sdh.move_bounding_sphere_to_origin(d_obj)
-    sdh.set_renderer_cycles_gpu()
-    sdh.set_background_white()
-    sdh.set_general_render_settings()
-    sdh.set_cycles_render_settings()
+    sphere_radius = sd.move_bounding_sphere_to_origin(d_obj)
+    sd.set_renderer_cycles_gpu()
+    sd.set_background_white()
+    sd.set_general_render_settings()
+    sd.set_cycles_render_settings()
     camera = bpy.context.scene.objects['Camera']
-    extrinsic_camera_data = sdh.random_render_on_sphere(
+    extrinsic_camera_data = sd.random_render_on_sphere(
         camera, bpy.context.scene, sphere_radius+3, 
         (0, 0, 0), args.num_renders, args.save_directory
     )
-    intrinsic_camera_data = sdh.get_intrinsic_camera_data()
-    transform_data = sdh.build_transform_data(intrinsic_camera_data, extrinsic_camera_data)
-    sdh.save_transform_data(transform_data, args.save_directory)
-    sdh.fetch_and_save_tri_count(args.save_directory)
+    intrinsic_camera_data = sd.get_intrinsic_camera_data()
+    transform_data = sd.build_transform_data(intrinsic_camera_data, extrinsic_camera_data)
+    sd.save_transform_data(transform_data, args.save_directory)
+    sd.fetch_and_save_tri_count(args.save_directory)
     bpy.ops.wm.save_as_mainfile(filepath='data/blend_files/test.blend')
 
 if __name__ == '__main__':

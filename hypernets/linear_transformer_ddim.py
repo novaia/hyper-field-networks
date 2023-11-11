545d97b2a14460a800697ad1709232c917cde506
hayden-donnelly <hayden.git@proton.me>
2023-11-10 20:07:29 -0500

    Linear transformer ddim training on anime faces.

--- hypernets/linear_transformer_ddim.py
+++ hypernets/linear_transformer_ddim.py
@@ -10,6 +10,7 @@ import random
 from functools import partial
 from hypernets.common.nn import SinusoidalEmbedding, LinearTransformer
 from hypernets.common.diffusion import diffusion_schedule, reverse_diffusion
+from hypernets.common.rendering import unpack_and_render_ngp_image
 
 class LinearTransformerDDIM(nn.Module):
     attention_dim:int
@@ -53,6 +54,7 @@ class LinearTransformerDDIM(nn.Module):
         x = x[:, :-1, :] # Remove embedded noise variances token.
         return x
 
+@jax.jit
 def train_step(state:TrainState, key:int, batch:jax.Array):
     noise_key, diffusion_time_key = jax.random.split(key)
 
@@ -70,11 +72,6 @@ def train_step(state:TrainState, key:int, batch:jax.Array):
     grad = jax.tree_util.tree_map(jnp.nan_to_num, grad)
     state = state.apply_gradients(grads=grad)
     return loss, state
-
-def save_image(image, file_path):
-    image = image - jnp.min(image)
-    image = image / jnp.max(image)
-    plt.imsave(file_path, image)
 
 def load_batch(sample_paths, batch_size, dtype):
     random.shuffle(sample_paths)
@@ -106,9 +103,10 @@ def inspect_intermediates(state, context_length, token_dim, dtype):
 
 def main():
     normal_dtype = jnp.float32
-    quantized_dtype = jnp.float16
+    quantized_dtype = jnp.float32
     batch_size = 1
-    datast_path = 'data/synthetic_nerfs/packed_aliens'
+    learning_rate = 5e-5
+    datast_path = 'data/packed_ngp_anime_faces'
     all_sample_paths = os.listdir(datast_path)
     valid_sample_paths = []
     for path in all_sample_paths:
@@ -126,53 +124,56 @@ def main():
     dummy_batch = load_batch_fn()
     print('batch shape', dummy_batch.shape)
     token_dim = dummy_batch.shape[-1]
-    #context_length = dummy_batch.shape[-2]
-    context_length = 168_000
+    context_length = dummy_batch.shape[-2]
+    print('context', context_length)
 
     model = LinearTransformerDDIM(
-        attention_dim=256,
+        attention_dim=128,
         token_dim=token_dim,
-        embedding_dim=256,
+        embedding_dim=128,
         num_bocks=2,
-        feed_forward_dim=256,
+        feed_forward_dim=128,
         embedding_max_frequency=1000.0,
         context_length=context_length,
         normal_dtype=normal_dtype,
         quantized_dtype=quantized_dtype
     )
     #tabulate(model, context_length, token_dim, quantized_dtype)
-    #exit(0)
 
-    tx = optax.adam(1e-3)
+    tx = optax.adam(learning_rate)
     rng = jax.random.PRNGKey(0)
     x = (jnp.ones((1, context_length, token_dim)), jnp.ones((1, 1, 1)))
     params = model.init(rng, x)['params']
     state = TrainState.create(apply_fn=model.apply, params=params, tx=tx)
 
-    train_steps = 100
+    train_steps = 20_000
     for step in range(train_steps):
         step_key = jax.random.PRNGKey(step)
         batch = load_batch_fn()
-        batch, extra = jnp.split(batch, axis=1, indices_or_sections=[context_length])
-        extra.delete()
         loss, state = train_step(state, step_key, batch)
-        batch.delete()
-        print('loss:', loss)
+        print('Step', step, 'Loss:', loss)
+        if step % 50 == 0 and step > 0:
+            generated_weights = reverse_diffusion(
+                state.apply_fn, 
+                state.params, 
+                num_images=1, 
+                diffusion_steps=20, 
+                context_length=context_length,
+                token_dim=token_dim,
+                diffusion_schedule_fn=diffusion_schedule,
+                min_signal_rate=0.02,
+                max_signal_rate=0.95,
+                seed=2
+            )
+            rendered_image = unpack_and_render_ngp_image(
+                config_path='configs/ngp_image.json',
+                weight_map_path='data/packed_ngp_anime_faces/weight_map.json',
+                packed_weights=generated_weights[0],
+                image_width=64,
+                image_height=64
+            )
+            plt.imsave(f'data/generations/ngp_image_step_{step}.png', rendered_image)
     print('Finished training')
-
-    generation = reverse_diffusion(
-        state.apply_fn, 
-        state.params, 
-        num_images=1, 
-        diffusion_steps=20, 
-        context_length=context_length,
-        token_dim=token_dim,
-        diffusion_schedule_fn=diffusion_schedule,
-        min_signal_rate=0.02,
-        max_signal_rate=0.95,
-        seed=2
-    )
-    save_image(generation[0], 'data/generation.png')
 
 if __name__ == '__main__':
     main()

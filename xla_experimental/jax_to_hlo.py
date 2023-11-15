73b6d19cae9453c1f4e928aa7e403235c5d71663
hayden-donnelly <hayden.git@proton.me>
2023-11-15 13:00:58 -0500

    Save hlo computation as serialized proto.
    The reason for this change comes from a line in the PJRT_Program
    definition in xla/pjrt/c/pjrt_c_api.h:
    
    struct PJRT_Program {
      size_t struct_size;
      void* priv;
      // Serialized code in the specified format below.
      // String is owned by the caller.
      char* code;  // in/out depending on usage
      size_t code_size;
      // Supported formats are:
      // "hlo": code string takes serialized HloModuleProto.
      // "hlo_with_config": code string takes serialized HloModuleProtoWithConfig.
      // "mlir": code string takes MLIR module bytecode (or string).
      // Ownership of `format` varies across API functions.
      const char* format;
      size_t format_size;
    };
    
    Specifically "code string takes serialized HloModuleProto".
    While I haven't tested this, it makes sense that the
    as_serialized_hlo_module_proto from jax.xla_computation is the right
    conversion here since the language matches. I kept to text serialization
    around since it's human readable, but I'll modify execute_hlo.cpp so
    that it loads the serialized proto instead. I also switched the
    target_fn to a simple square since it will be a bit easier to eyeball
    correctness once I get the HLO running with PJRT.

--- xla_experimental/jax_to_hlo.py
+++ xla_experimental/jax_to_hlo.py
@@ -2,12 +2,18 @@ import jax
 import jax.numpy as jnp
 
 def target_fn(x):
-    return jnp.sin(x)
+    return x**2
 
 def main():
     hlo_comp = jax.xla_computation(target_fn)(jnp.ones((1,)))
-    with open('hlo_comp.txt', 'w') as f:
-        f.write(hlo_comp.as_hlo_text())
+    hlo_comp_proto = str(hlo_comp.as_serialized_hlo_module_proto())
+    hlo_comp_text = hlo_comp.as_hlo_text()
+    with open('hlo_comp_proto.txt', 'w') as f:
+        f.write(hlo_comp_proto)
+    with open('hlo_comp_text.txt', 'w') as f:
+        f.write(hlo_comp_text)
+    
+    print('Saved the following HLO computation:\n')
     print(hlo_comp.as_hlo_text())
 
 if __name__ == '__main__':

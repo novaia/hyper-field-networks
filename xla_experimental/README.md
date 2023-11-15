b48236a95240d6a4c3d74499a8a531cdc9dea86b
hayden-donnelly <hayden.git@proton.me>
2023-11-15 22:13:22 +0000

    Working execution with pjrt runtime.

--- xla_experimental/README.md
+++ xla_experimental/README.md
@@ -2,35 +2,7 @@
 
 The purpose of this directory is to experiment with calling JAX generated XLA HLO from C++ in order to cut out Python overhead and speedup loops.
 
-## XLA Extension
-
-Download pre-compiled XLA extension:
-```
-wget https://github.com/elixir-nx/xla/releases/download/v0.6.0/xla_extension-x86_64-linux-gnu-cuda120.tar.gz
-```
-
-Extract it:
-```
-tar -xvzf ./xla_extension-x86_64-linux-gnu-cuda120.tar.gz
-```
-
-Delete the archive file:
-```
-rm ./xla_extension-x86_64-linux-gnu-cuda120.tar.gz
-```
-
-Compile execute_hlo.cpp:
-
-```
-cmake . -B build
-```
-```
-make -C build
-```
-
-## PJRT Runtime
-
-The XLA extension process defined above didn't work, so I'm going to try building the PJRT runtime from the main XLA repository.
+## Executing HLO with PJRT Runtime
 
 Install Bazelisk:
 ```
@@ -54,37 +26,12 @@ Clone XLA repository:
 git clone https://github.com/openxla/xla.git
 ```
 
-Change into XLA directory:
+Build execute_hlo with XLA dependencies:
 ```
-cd xla
-```
-
-Configure build:
-```
-yes '' | GCC_HOST_COMPILER_PATH=/usr/bin/gcc-10 CC=/usr/bin/gcc-10 TF_NEED_ROCM=0 TF_NEED_CUDA=1 TF_CUDA_CLANG=0 ./configure
-```
-
-Build PJRT runtime:
-```
-bazel build --strip=never /bazel-xla:literal //xla:literal_util //xla:shape_util //xla:status //xla:statusor //xla/pjrt:pjrt_client //xla/pjrt:tfrt_cpu_pjrt_client //xla/service:hlo_proto_cc //xla/tools:hlo_module_loader
-```
-
-//tsl/platform:logging //tsl/platform:platform_port
-
-Compile execute_hlo.cpp
-```
-cmake . -B build
-```
-```
-make -C build
+bazel build //:execute_hlo
 ```
 
 Run execute_hlo:
 ```
-./build/execute_hlo
-```
-
-Single command to compile and run (assumes build files have already been written):
-```
-make -C build && ./build/execute_hlo
+bazel-bin/execute_hlo
 ```
--- xla_experimental/execute_hlo.cpp
+++ xla_experimental/execute_hlo.cpp
@@ -16,11 +16,43 @@
 
 int main(int argc, char** argv) 
 {
-    std::ifstream t("hlo_comp_proto.txt");
-    std::stringstream buffer;
-    buffer << t.rdbuf();
-    std::string hlo_code = buffer.str();
-    std::string format = "hlo";
-    std::cout << hlo_code << "\n";
+    // Load HloModule from file.
+    std::string hlo_filename = "hlo_comp_text.txt";
+    std::function<void(xla::HloModuleConfig*)> config_modifier_hook =
+        [](xla::HloModuleConfig* config) { config->set_seed(42); };
+    std::unique_ptr<xla::HloModule> test_module = 
+        LoadModuleFromFile(
+            hlo_filename, xla::hlo_module_loader_details::Config(),
+            "txt", config_modifier_hook
+        ).value();
+    const xla::HloModuleProto test_module_proto = test_module->ToProto();
+
+    // Run it using JAX C++ Runtime (PJRT).
+
+    // Get a CPU client.
+    std::unique_ptr<xla::PjRtClient> client =
+        xla::GetTfrtCpuClient(/*asynchronous=*/true).value();
+
+    // Compile XlaComputation to PjRtExecutable.
+    xla::XlaComputation xla_computation(test_module_proto);
+    xla::CompileOptions compile_options;
+    std::unique_ptr<xla::PjRtLoadedExecutable> executable =
+        client->Compile(xla_computation, compile_options).value();
+
+    // Prepare input.
+    xla::Literal literal_x =
+        xla::LiteralUtil::CreateR2<float>({{2.0f}});
+    std::unique_ptr<xla::PjRtBuffer> param_x =
+        client->BufferFromHostLiteral(literal_x, client->addressable_devices()[0]).value();
+
+    // Execute on CPU.
+    xla::ExecuteOptions execute_options;
+    // One vector<buffer> for each device.
+    std::vector<std::vector<std::unique_ptr<xla::PjRtBuffer>>> results =
+        executable->Execute({{param_x.get()}}, execute_options).value();
+
+    // Get result.
+    std::shared_ptr<xla::Literal> result_literal = results[0][0]->ToLiteralSync().value();
+    std::cout << "Result = " << *result_literal << "\n";
     return 0;
 }

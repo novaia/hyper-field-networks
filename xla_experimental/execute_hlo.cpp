5be4d3f73cb07677745bbdc0a7c24b0b915b126e
hayden-donnelly <hayden.git@proton.me>
2023-11-14 23:38:25 -0500

    Initialize pjrt program struct.

--- xla_experimental/execute_hlo.cpp
+++ xla_experimental/execute_hlo.cpp
@@ -12,6 +12,18 @@ int main(int argc, char** argv)
     std::ifstream t("hlo_comp.txt");
     std::stringstream buffer;
     buffer << t.rdbuf();
-    std::cout << buffer.str() << "\n";
+    std::string hlo_code = buffer.str();
+    std::string format = "hlo";
+    
+    struct PJRT_Program pjrt_program;
+    pjrt_program.code = (char*)hlo_code.c_str();
+    pjrt_program.code_size = (size_t)hlo_code.size();
+    pjrt_program.format = format.c_str();
+    pjrt_program.format_size = (size_t)format.size();
+
+    std::cout << "HLO Code:\n\n" << pjrt_program.code;
+    std::cout << "Code size: " << pjrt_program.code_size << "\n";
+    std::cout << "Format: " << pjrt_program.format << "\n";
+    std::cout << "Format size: " << pjrt_program.format_size << "\n";
     return 0;
 }

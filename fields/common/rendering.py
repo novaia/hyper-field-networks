240ea0e2942533c35321d651c3cfafb78f1122a2
hayden-donnelly <hayden.git@proton.me>
2023-11-16 14:04:50 -0500

    Deleted fields rendering module.

--- fields/common/rendering.py
+++
@@ -1,8 +0,0 @@
-import jax
-import jax.numpy as jnp
-
-def straight_alpha_composite(foreground:jax.Array, background:jax.Array, alpha:jax.Array):
-    return foreground * alpha + background * (1 - alpha)
-
-def alpha_composite(foreground:jax.Array, background:jax.Array, alpha:jax.Array):
-    return foreground + background * (1 - alpha)

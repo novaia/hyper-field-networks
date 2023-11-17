599933da54ba5b4c9a02d2b1e5a433c37f7b5f8a
hayden-donnelly <hayden.git@proton.me>
2023-11-17 16:32:03 -0500

    Added train multiple module for image field.

--- fields/image_field.py
+++ fields/image_field.py
@@ -11,6 +11,9 @@ from PIL import Image
 from functools import partial
 import matplotlib.pyplot as plt
 from fields.common.nn import FrequencyEncoding, FeedForward
+import os
+import json
+import argparse
 
 class ImageField(nn.Module):
     mlp_depth:int
@@ -27,12 +30,14 @@ class ImageField(nn.Module):
         x = nn.activation.sigmoid(x)
         return x
 
-def train_loop(image, image_width, image_height, batch_size, steps, state):
+def train_loop(image, batch_size, steps, state, return_final_loss):
+    image_height = image.shape[0]
+    image_width = image.shape[1]
     for step in range(steps):
         rng = jax.random.PRNGKey(step)
         loss, state = train_step(image, image_width, image_height, batch_size, state, rng)
-        #print('Loss:', loss)
-    print('Final loss:', loss)
+    if return_final_loss:
+        return state, loss
     return state
 
 @partial(jax.jit, static_argnames=('batch_size', 'image_width', 'image_height'))
@@ -67,7 +72,7 @@ def create_train_state(model, learning_rate, rng):
     ts = TrainState.create(apply_fn=model.apply, params=params, tx=tx)
     return ts
 
-def draw_image(state, image_width, image_height, file_path):
+def render_image(state, image_width, image_height):
     x = jnp.stack(jnp.meshgrid(
         jnp.linspace(0, 1, image_width),
         jnp.linspace(0, 1, image_height)
@@ -77,19 +82,34 @@ def draw_image(state, image_width, image_height, file_path):
     image = jnp.reshape(image, (image_width, image_height, 3))
     image = jnp.clip(image, 0, 1)
     image = jnp.transpose(image, (1, 0, 2))
-    plt.imsave(file_path, image)
+    return image
+
+def create_model_from_config(config:dict):
+    model = ImageField(
+        mlp_depth=config['mlp_depth'], 
+        mlp_width=config['mlp_width'], 
+        encoding_dim=config['encoding_dim']
+    )
+    return model
 
 def main():
-    image_path = 'data/approximation_field/approximation_test.png'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='configs/image_field.json')
+    parser.add_argument('--input_path', type=str, default='data/CIFAR10/testairplane0596.jpg')
+    args = parser.parse_args()
+    assert os.path.isfile(args.input_path)
+
+    image_path = 'data/CIFAR10/testairplane0596.jpg'
     image = Image.open(image_path)
     image = jnp.array(image) / 255.0
 
-    model = ImageField(mlp_depth=3, mlp_width=256, encoding_dim=10)
+    with open(args.config) as f:
+        config = json.load(f)
+
+    model = create_model_from_config(config)
     state = create_train_state(model, 1e-3, jax.random.PRNGKey(0))
     state = train_loop(image, image.shape[0], image.shape[1], 32, 1000, state)
-    draw_image(
-        state, image.shape[0], image.shape[1], 'data/approximation_field/approximation.png'
-    )
+    render_image(state, image.shape[0], image.shape[1], 'data/image_field_output.png')
 
 if __name__ == '__main__':
     main()

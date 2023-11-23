7ef491e3ae6a94991868b5bd3530010e8a08333c
hayden-donnelly <hayden.git@proton.me>
2023-11-22 21:10:45 -0500

    Switched to bce and kld implementations from google research.
    The google research binary cross entropy function takes logvars instead
    of standard deviations, so I modified the vae to account for this.

--- hypernets/transformer_vae.py
+++ hypernets/transformer_vae.py
@@ -10,6 +10,15 @@ from nvidia.dali.plugin.jax import DALIGenericIterator
 import matplotlib.pyplot as plt
 import os
 
+@jax.vmap
+def kl_divergence(mean, logvar):
+    return -0.5 * jnp.sum(1 + logvar - jnp.square(mean) - jnp.exp(logvar))
+
+@jax.vmap
+def binary_cross_entropy_with_logits(logits, labels):
+    logits = nn.log_sigmoid(logits)
+    return -jnp.sum(labels * logits + (1.0 - labels) * jnp.log(-jnp.expm1(logits)))
+
 class AutoPositionalEmbedding(nn.Module):
     num_positions: int
     feature_dim: int
@@ -49,9 +58,8 @@ class TransformerEncoder(nn.Module):
         flattened_shape = (x.shape[0], self.context_length * x.shape[-1])
         x = jnp.reshape(x, flattened_shape)
         means = nn.Dense(self.latent_dim)(x)
-        deviations = nn.Dense(self.latent_dim)(x)
-        deviations = jnp.exp(deviations)
-        return means, deviations
+        logvars = nn.Dense(self.latent_dim)(x)
+        return means, logvars
     
 class TransformerDecoder(nn.Module):
     context_length: int
@@ -92,11 +100,11 @@ class TransformerVae(nn.Module):
     @nn.compact
     def __call__(self, x):
         x, key = x
-        means, deviations = self.encoder(x)
-        x = means + deviations * jax.random.normal(key, means.shape)
+        means, logvars = self.encoder(x)
+        x = means + jnp.exp(0.5 * logvars) * jax.random.normal(key, means.shape)
         logits = self.decoder(x)
-        return logits, means, deviations
-    
+        return logits, means, logvars
+
 def create_transformer_vae(
     hidden_dims, latent_dim, output_dim, context_length, num_attention_heads
 ):
@@ -145,9 +153,9 @@ def get_data_iterator(dataset_path, batch_size, num_threads=3):
 def train_step(state, batch, kl_weight, batch_size):
     key = jax.random.PRNGKey(state.step)
     def loss_fn(params):
-        logits, means, deviations = state.apply_fn({'params': params}, [batch, key])
-        bce_loss = jnp.sum(optax.sigmoid_binary_cross_entropy(logits, batch)) / batch_size
-        kld_loss = jnp.sum(deviations**2 + means**2 - jnp.log(deviations) - 0.5) / batch_size
+        logits, means, logvars = state.apply_fn({'params': params}, [batch, key])
+        bce_loss = jnp.mean(binary_cross_entropy_with_logits(logits, batch))
+        kld_loss = jnp.mean(kl_divergence(means, logvars))
         loss = bce_loss * (1 - kl_weight) + kld_loss * (kl_weight)
         return loss, (bce_loss, kld_loss)
     grad_fn = jax.value_and_grad(loss_fn, has_aux=True)

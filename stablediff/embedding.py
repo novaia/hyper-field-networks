cbc5715898089d49f52ed5884126a97a876e3892
hayden-donnelly <hayden.git@proton.me>
2023-11-24 16:23:40 -0500

    Untested refactors of stablediff embeddings.

--- stablediff/embedding.py
+++ stablediff/embedding.py
@@ -3,7 +3,34 @@ import math
 import flax.linen as nn
 import jax.numpy as jnp
 
+# Refactored - done but untested
+def get_sinusoidal_embeddings(
+    timesteps: jnp.ndarray,
+    embedding_dim: int,
+    freq_shift: float = 1,
+    min_timescale: float = 1,
+    max_timescale: float = 1.0e4,
+    flip_sin_to_cos: bool = False,
+    scale: float = 1.0,
+) -> jnp.ndarray:
+    assert timesteps.ndim == 1, "Timesteps should be a 1d-array"
+    assert embedding_dim % 2 == 0, f"Embedding dimension {embedding_dim} should be even"
+    num_timescales = float(embedding_dim // 2)
+    log_timescale_increment = math.log(max_timescale / min_timescale) / (num_timescales - freq_shift)
+    inv_timescales = min_timescale * jnp.exp(jnp.arange(num_timescales, dtype=jnp.float32) * -log_timescale_increment)
+    emb = jnp.expand_dims(timesteps, 1) * jnp.expand_dims(inv_timescales, 0)
 
+    # scale embeddings
+    scaled_time = scale * emb
+
+    if flip_sin_to_cos:
+        signal = jnp.concatenate([jnp.cos(scaled_time), jnp.sin(scaled_time)], axis=1)
+    else:
+        signal = jnp.concatenate([jnp.sin(scaled_time), jnp.cos(scaled_time)], axis=1)
+    signal = jnp.reshape(signal, [jnp.shape(timesteps)[0], embedding_dim])
+    return signal
+
+# Replaced
 def get_sinusoidal_embeddings(
     timesteps: jnp.ndarray,
     embedding_dim: int,
@@ -40,7 +67,19 @@ def get_sinusoidal_embeddings(
     signal = jnp.reshape(signal, [jnp.shape(timesteps)[0], embedding_dim])
     return signal
 
+# Refactored - done but untested
+class TimestepEmbedding(nn.Module):
+    time_embed_dim: int = 32
+    dtype: jnp.dtype = jnp.float32
 
+    @nn.compact
+    def __call__(self, temb):
+        temb = nn.Dense(self.time_embed_dim, dtype=self.dtype, name="linear_1")(temb)
+        temb = nn.silu(temb)
+        temb = nn.Dense(self.time_embed_dim, dtype=self.dtype, name="linear_2")(temb)
+        return temb
+
+# Replaced by TimestepEmbedding
 class FlaxTimestepEmbedding(nn.Module):
     r"""
     Time step Embedding Module. Learns embeddings for input time steps.
@@ -61,7 +100,24 @@ class FlaxTimestepEmbedding(nn.Module):
         temb = nn.Dense(self.time_embed_dim, dtype=self.dtype, name="linear_2")(temb)
         return temb
 
+# Refactored - done but untested
+# Wrapper module for sinusoial time step embeddings as described in:
+# https://arxiv.org/abs/2006.11239
+class SinusoidalTimestepEmbedding(nn.Module):
+    dim: int = 32
+    flip_sin_to_cos: bool = False
+    freq_shift: float = 1
 
+    @nn.compact
+    def __call__(self, timesteps):
+        return get_sinusoidal_embeddings(
+            timesteps, 
+            embedding_dim=self.dim, 
+            flip_sin_to_cos=self.flip_sin_to_cos, 
+            freq_shift=self.freq_shift
+        )
+
+# Replaced by SinusoidalTimestepEmbedding
 class FlaxTimesteps(nn.Module):
     r"""
     Wrapper Module for sinusoidal Time step Embeddings as described in https://arxiv.org/abs/2006.11239

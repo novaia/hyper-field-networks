a44878e6f0822909fcc0e322ab41a1650adf1062
hayden-donnelly <hayden.git@proton.me>
2023-11-26 13:46:07 -0500

    Various fixes to basic gan.
    - It is unnecessary to give the discriminator real and fake data when
      training the generator. Now only fake data is given.
    
    - The generator loss should be calculated with real labels instead of
      fake labels in order to reward the generator for fooling the
      discriminator.
    
    - Added num_groups parameter for more control over group norm.
    
    - Switched latent to 1-dimensional vector and reshaped within generator.
    
    - Scale data from [0, 255] to [0, 1] inside train_step so that it's
      compiled.
    
    - Switched to sum of binary cross entropy loss, unclear if this makes a
      difference or if effects have been confounded with other changes.
    
    - Moved preview generation into separate function to clean up train
      loop.

--- gigagan/basic_gan.py
+++ gigagan/basic_gan.py
@@ -13,6 +13,7 @@ import os
 class ResidualBlock(nn.Module):
     width: int
     activation: Callable
+    num_groups: int = 32
 
     @nn.compact
     def __call__(self, x):
@@ -22,10 +23,10 @@ class ResidualBlock(nn.Module):
         else:
             residual = nn.Conv(self.width, kernel_size=(1, 1))(x)
         x = nn.Conv(self.width, kernel_size=(3, 3))(x)
-        x = nn.GroupNorm()(x)
+        x = nn.GroupNorm(num_groups=self.num_groups)(x)
         x = self.activation(x)
         x = nn.Conv(self.width, kernel_size=(3, 3))(x)
-        x = nn.GroupNorm()(x)
+        x = nn.GroupNorm(num_groups=self.num_groups)(x)
         x = self.activation(x)
         x = x + residual
         return x
@@ -33,14 +34,17 @@ class ResidualBlock(nn.Module):
 class Generator(nn.Module):
     widths: List[int]
     block_depth: int
+    num_groups: int = 32
 
     @nn.compact
     def __call__(self, x):
+        x = nn.Dense(8**2)(x)
+        x = jnp.reshape(x, (x.shape[0], 8, 8, 1))
         for width in self.widths:
             upsample_shape = (x.shape[0], x.shape[1] * 2, x.shape[2] * 2, x.shape[3])
             x = jax.image.resize(x, upsample_shape, method='bilinear')
             for _ in range(self.block_depth):
-                x = ResidualBlock(width, activation=nn.relu)(x)
+                x = ResidualBlock(width, activation=nn.relu, num_groups=self.num_groups)(x)
         x = nn.Conv(3, kernel_size=(1, 1))(x)
         x = nn.sigmoid(x)
         return x
@@ -48,55 +52,56 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     widths: List[int]
     block_depth: int
+    num_groups: int = 32
 
     @nn.compact
     def __call__(self, x):
         for width in self.widths:
             for _ in range(self.block_depth):
-                x = ResidualBlock(width, activation=nn.leaky_relu)(x)
+                x = ResidualBlock(
+                    width, activation=nn.leaky_relu, num_groups=self.num_groups
+                )(x)
             x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
         x = nn.Conv(1, kernel_size=(1, 1))(x)
         x = jnp.squeeze(x, axis=-1)
-        x = jnp.mean(x, axis=(-1, -2))
+        x = nn.DenseGeneral(1, axis=(-1, -2))(x)
         return x
 
-@partial(jax.jit, static_argnames=['latent_width', 'latent_height'])
-def train_step(
-    generator_state, discriminator_state, real_data, latent_width, latent_height, key
-):
-    latent_noise = jax.random.normal(key, (real_data.shape[0], latent_height, latent_width, 1))
-    real_labels = jnp.ones((real_data.shape[0]))
-    fake_labels = jnp.zeros((real_data.shape[0]))
-    labels = jnp.concatenate([real_labels, fake_labels], axis=0)
+@partial(jax.jit, static_argnames=['latent_dim', 'batch_size'])
+def train_step(generator_state, discriminator_state, real_data, key, latent_dim, batch_size):
+    real_data = real_data / 255.
+    latent_noise = jax.random.normal(key, (batch_size, latent_dim))
+    real_labels = jnp.ones((batch_size, 1))
+    fake_labels = jnp.zeros((batch_size, 1))
+    real_and_fake_labels = jnp.concatenate([real_labels, fake_labels], axis=0)
 
-    def generator_loss_fn(params):
-        fake_data = generator_state.apply_fn(
-            {'params': params}, latent_noise
-        )
-        real_and_fake_data = jnp.concatenate([real_data, fake_data], axis=0)
-        logits = discriminator_state.apply_fn(
-            {'params': discriminator_state.params}, real_and_fake_data
-        )
-        loss = jnp.mean(optax.sigmoid_binary_cross_entropy(logits, labels))
-        return loss
-    generator_grad_fn = jax.value_and_grad(generator_loss_fn)
-    generator_loss, generator_grads = generator_grad_fn(generator_state.params)
-    generator_state = generator_state.apply_gradients(grads=generator_grads)
-
+    fake_data = generator_state.apply_fn(
+        {'params': generator_state.params}, latent_noise
+    )
+    real_and_fake_data = jnp.concatenate([real_data, fake_data], axis=0)
     def discriminator_loss_fn(params):
-        fake_data = generator_state.apply_fn(
-            {'params': generator_state.params}, latent_noise
-        )
-        real_and_fake_data = jnp.concatenate([real_data, fake_data], axis=0)
         logits = discriminator_state.apply_fn(
             {'params': params}, real_and_fake_data
         )
-        loss = jnp.mean(optax.sigmoid_binary_cross_entropy(logits, labels))
+        loss = jnp.sum(optax.sigmoid_binary_cross_entropy(logits, real_and_fake_labels))
         return loss
     discriminator_grad_fn = jax.value_and_grad(discriminator_loss_fn)
     discriminator_loss, discriminator_grads = discriminator_grad_fn(discriminator_state.params)
     discriminator_state = discriminator_state.apply_gradients(grads=discriminator_grads)
 
+
+    def generator_loss_fn(params):
+        fake_data = generator_state.apply_fn(
+            {'params': params}, latent_noise
+        )
+        logits = discriminator_state.apply_fn(
+            {'params': discriminator_state.params}, fake_data
+        )
+        loss = jnp.sum(optax.sigmoid_binary_cross_entropy(logits, real_labels))
+        return loss
+    generator_grad_fn = jax.value_and_grad(generator_loss_fn)
+    generator_loss, generator_grads = generator_grad_fn(generator_state.params)
+    generator_state = generator_state.apply_gradients(grads=generator_grads)
     return (generator_loss, discriminator_loss), generator_state, discriminator_state
 
 def get_data_iterator(dataset_path, batch_size, num_threads=3):
@@ -114,25 +119,36 @@ def get_data_iterator(dataset_path, batch_size, num_threads=3):
     iterator = DALIGenericIterator(pipelines=[my_pipeline], output_map=['x'], reader_name='r')
     return iterator
 
+def generate_and_save_previews(latent_dim, generator_state, save_path):
+    latent_shape = (4, latent_dim)
+    latent_key = jax.random.PRNGKey(generator_state.step)
+    latent_input = jax.random.normal(latent_key, latent_shape)
+    generated_images = generator_state.apply_fn(
+        {'params': generator_state.params}, latent_input
+    )
+    for i in range(generated_images.shape[0]):
+        plt.imsave(
+            os.path.join(save_path, f'step{generator_state.step}_image{i}.png'), 
+            generated_images[i]
+        )
+
 def main():
-    latent_height = 8
-    latent_width = 8
-    batch_size = 32
+    latent_dim = 128
+    batch_size = 64
     num_epochs = 10
+    learning_rate = 3e-4
     dataset_path = 'data/cifar10_numpy'
     save_path = 'data/gan_generations'
-    generator = Generator(widths=[32, 32], block_depth=2)
-    discriminator = Discriminator(widths=[32, 32], block_depth=2)
-
-    learning_rate = 3e-4
-    optimizer = optax.adam(learning_rate)
+    generator = Generator(widths=[8, 8], block_depth=2, num_groups=2)
+    discriminator = Discriminator(widths=[8, 8], block_depth=2, num_groups=2)
 
     key = jax.random.PRNGKey(0)
-    latent_input = jnp.ones((1, latent_height, latent_width, 1))
+    latent_input = jnp.ones((1, latent_dim))
     generator_output, generator_variables = generator.init_with_output(key, latent_input)
     generator_params = generator_variables['params']
+    generator_tx = optax.adam(learning_rate)
     generator_state = TrainState.create(
-        apply_fn=generator.apply, params=generator_params, tx=optimizer
+        apply_fn=generator.apply, params=generator_params, tx=generator_tx
     )
     print('Generator Output:', generator_output.shape)
 
@@ -140,37 +156,29 @@ def main():
         key, generator_output
     )
     discriminator_params = discriminator_variables['params']
+    discriminator_tx = optax.adam(learning_rate)
     discriminator_state = TrainState.create(
-        apply_fn=discriminator.apply, params=discriminator_params, tx=optimizer
+        apply_fn=discriminator.apply, params=discriminator_params, tx=discriminator_tx
     )
     print('Discriminator Output:', discriminator_output.shape)
 
     data_iterator = get_data_iterator(dataset_path, batch_size)
+    loss_message = 'Gen Loss: {:>8.5f}\tDisc Loss: {:>8.5f}'
     for epoch in range(num_epochs):
-        for step, batch in enumerate(data_iterator):
-            batch = batch['x']/255.
+        for batch in data_iterator:
+            batch = batch['x']
             losses, generator_state, discriminator_state = train_step(
                 discriminator_state=discriminator_state,
                 generator_state=generator_state,
                 real_data=batch,
-                latent_width=latent_width,
-                latent_height=latent_height,
+                latent_dim=latent_dim,
+                batch_size=batch_size,
                 key=jax.random.PRNGKey(generator_state.step)
             )
             generator_loss, disciminator_loss = losses
-            print('Gen Loss:', generator_loss, 'Disc Loss:', disciminator_loss)
-            if step % 100 == 0 and step > 0:
-                latent_shape = (4, latent_height, latent_width, 1)
-                latent_key = jax.random.PRNGKey(generator_state.step)
-                latent_input = jax.random.normal(latent_key, latent_shape)
-                generated_images = generator_state.apply_fn(
-                    {'params': generator_state.params}, latent_input
-                )
-                for i in range(generated_images.shape[0]):
-                    plt.imsave(
-                        os.path.join(save_path, f'epoch{epoch}_step{step}_image{i}.png'), 
-                        generated_images[i]
-                    )
+            print(loss_message.format(generator_loss, disciminator_loss))
+            if generator_state.step % 100 == 0 and generator_state.step > 0:
+                generate_and_save_previews(latent_dim, generator_state, save_path)   
         print(f'Finished epoch {epoch}')
 
 

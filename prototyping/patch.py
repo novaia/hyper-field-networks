44134e9e647938c51a6c6c27bb34149eb7b4929b
hayden-donnelly <hayden.git@proton.me>
2023-11-29 17:32:53 -0500

    Patchify with patch flattening.

--- prototyping/patch.py
+++ prototyping/patch.py
@@ -13,7 +13,8 @@ def patchify(x, patch_size):
     def get_patch(x, horizontal_id, vertical_id):
         start_indices = [horizontal_id, vertical_id, 0]
         slice_sizes = [patch_size, patch_size, 3]
-        return lax.dynamic_slice(x, start_indices, slice_sizes)
+        patch = lax.dynamic_slice(x, start_indices, slice_sizes)
+        return jnp.ravel(patch)
 
     num_patches_across = (x.shape[1] // patch_size)
     num_patches_total = num_patches_across**2 
@@ -22,21 +23,25 @@ def patchify(x, patch_size):
     patches = jax.vmap(
         jax.vmap(get_patch, in_axes=(None, 0, 0)), in_axes=(None, 0, 0)
     )(x, horizontal_indices, vertical_indices)
-    patches = jnp.reshape(patches, (num_patches_total, patch_size, patch_size, 3))
-    return patches, num_patches_across
+    patches = jnp.reshape(patches, (num_patches_total, patch_size*patch_size*3))
+    return patches, num_patches_across, num_patches_total
 
-def depatchify(x, num_patches_across):
+@partial(jax.jit, static_argnames=['patch_size', 'num_patches_across'])
+def depatchify(x, patch_size, num_patches_across, num_patches_total):
+    x = jnp.reshape(x, (num_patches_across**2, patch_size, patch_size, 3))
     x = jnp.array(jnp.split(x, num_patches_across, axis=0))
     x = jax.vmap(lambda a: jnp.concatenate(a, axis=0), in_axes=0)(x)
     x = jnp.concatenate(x, axis=1)
     return x
 
-patches, num_patches_across = patchify(image, 8)
-print('patches', patches.shape)
-for i in range(patches.shape[0]):
-    plt.imsave(f'data/patches/{i}.png', patches[i])
+patch_size = 8
+patches, num_patches_across, num_patches_total = patchify(image, patch_size)
+#print('patches', patches.shape)
+#for i in range(patches.shape[0]):
+#    plt.imsave(f'data/patches/{i}.png', patches[i])
 
-reconstructed = depatchify(patches, num_patches_across)
+num_patches_across = int(num_patches_across)
+reconstructed = depatchify(patches, patch_size, num_patches_across, num_patches_total)
 print('num patches across', num_patches_across)
 print('reconstructed', reconstructed.shape)
 plt.imsave('data/patches/reconstructed.png', reconstructed)

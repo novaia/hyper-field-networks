9f8d7baf516a1ece8d974dc5ee097f875c3b73a2
hayden-donnelly <hayden.git@proton.me>
2023-12-21 21:21:56 -0500

    Added premade eevee synthdata script.
    Hard coded paths for convenience at the moment. Will need to be improved
    and merged into the other premade script.

---
+++ synthdata/blender_scripts/premade_eevee.py
@@ -0,0 +1,29 @@
+import bpy
+import os
+import sys
+print(os.getcwd())
+sys.path.append(os.getcwd())
+import synthdata.common as sd
+
+def main():
+    args = sd.get_arguments()
+    bpy.ops.wm.open_mainfile(filepath='data/blend_files/female_base_2.blend')
+    bpy.ops.object.camera_add()
+    bpy.context.scene.camera = bpy.context.object
+    d_obj = bpy.context.scene.objects['Body']
+    sphere_radius = sd.move_bounding_sphere_to_origin(d_obj)
+    bpy.data.scenes['Scene'].render.engine = 'BLENDER_EEVEE'
+    sd.set_general_render_settings()
+    camera = bpy.context.scene.objects['Camera']
+    extrinsic_camera_data = sd.random_render_on_sphere(
+        camera, bpy.context.scene, sphere_radius+3, 
+        (0, 0, 0), args.num_renders, args.save_directory
+    )
+    intrinsic_camera_data = sd.get_intrinsic_camera_data()
+    transform_data = sd.build_transform_data(intrinsic_camera_data, extrinsic_camera_data)
+    sd.save_transform_data(transform_data, args.save_directory)
+    #sd.fetch_and_save_tri_count(args.save_directory)
+    #bpy.ops.wm.save_as_mainfile(filepath='data/blend_files/test.blend')
+
+if __name__ == '__main__':
+    main()

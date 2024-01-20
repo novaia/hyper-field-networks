6f11d187a7620408e116bf2041200f3a8137857a
hayden-donnelly <hayden.git@proton.me>
2024-01-20 14:34:17 -0500

    Deleted unet_conditional init_weights member function.
    Weights/params are now initialized with a function outside of the model
    class.

--- diffusion/hf_diffusers/models/unet_conditional.py
+++ diffusion/hf_diffusers/models/unet_conditional.py
@@ -80,18 +80,6 @@ class UNet2dConditionalModel(nn.Module):
     flip_sin_to_cos: bool = True
     freq_shift: int = 0
 
-    def init_weights(self, rng: jax.random.PRNGKey) -> Dict:
-        # init input tensors
-        sample_shape = (1, self.in_channels, self.sample_size, self.sample_size)
-        sample = jnp.zeros(sample_shape, dtype=jnp.float32)
-        timesteps = jnp.ones((1,), dtype=jnp.int32)
-        encoder_hidden_states = jnp.zeros((1, 1, self.cross_attention_dim), dtype=jnp.float32)
-
-        params_rng, dropout_rng = jax.random.split(rng)
-        rngs = {"params": params_rng, "dropout": dropout_rng}
-
-        return self.init(rngs, sample, timesteps, encoder_hidden_states)["params"]
-
     @nn.compact
     def __call__(
         self,

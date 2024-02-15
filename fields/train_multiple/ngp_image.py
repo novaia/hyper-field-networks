592cb10cef90dbd5a29e82ca5fa33fb7d4369de5
hayden-donnelly <hayden.git@proton.me>
2024-02-15 18:30:48 -0500

    Fixed bucket path.

--- fields/train_multiple/ngp_image.py
+++ fields/train_multiple/ngp_image.py
@@ -27,7 +27,7 @@ def train(input_path:str, output_path:str, config:dict, render:bool):
         split_path = path.split('/')
         file_name = split_path[-1]
         bucket_name = split_path[-2]
-        output_bucket_path = f'{output_path}/bucket_name'
+        output_bucket_path = f'{output_path}/{bucket_name}'
         output_file_name = os.path.join(output_bucket_path, f'{file_name[:-4]}.npy')
         if output_file_name in output_path_list:
             print(f'Input file {path} already exists in output as {output_name}, skipping...')

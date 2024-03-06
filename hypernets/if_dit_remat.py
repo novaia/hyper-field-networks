bcf89a34053a4ddf7a392f942bbee9ccf8eda599
hayden-donnelly <hayden.git@proton.me>
2024-03-05 21:38:06 -0500

    Changed if_dit_remat output dir.

--- hypernets/if_dit_remat.py
+++ hypernets/if_dit_remat.py
@@ -259,7 +259,7 @@ def get_data_iterator(dataset_path, token_dim, batch_size, num_threads=4):
     return data_iterator, num_batches, context_length
 
 def main():
-    output_dir = 'data/if_dit_runs/6'
+    output_dir = 'data/if_dit_runs/7'
     config_path = 'configs/if_dit_remat.json'
     field_config_path = 'configs/ngp_image.json'
     dataset_path = 'data/mnist_ingp_flat'

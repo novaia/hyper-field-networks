7653fa3a3323475f2dbcd7d474a09613630a0334
hayden-donnelly <hayden.git@proton.me>
2024-03-22 01:45:20 -0400

    Fixed styling of nix code in dependencies.

--- dependencies/default.nix
+++ dependencies/default.nix
@@ -11,13 +11,13 @@ let
             (filterAttrs
                 (name: type: type == "directory" && name != "_sources")
                 (readDir basedir));
-    # Compute capability, used for building tiny-cuda-nn
-    # NOTE: removing unused compute capabilities will build faster, GPUs and their compute
-    # capabilities can be found at:
-    #   <https://arnon.dk/matching-sm-architectures-arch-and-gencode-for-various-nvidia-cards/>
-    # All the compute capabilities since `5.0`. REF: <https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#features-and-technical-specifications-technical-specifications-per-compute-capability>
+    # Compute capability, used for building tiny-cuda-nn,
+    # NOTE: removing unused compute capabilities will build faster. 
+    # GPUs and their compute capabilities can be found at:
+    # https://arnon.dk/matching-sm-architectures-arch-and-gencode-for-various-nvidia-cards
+    # All the compute capabilities since `5.0`. REF: 
+    # https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#features-and-technical-specifications-technical-specifications-per-compute-capability
     cudaCapabilities = [
-        # /nix/store/hsbbv8a72hwjrka8igd7hk66skvi03rp-cudatoolkit-11.7.0-unsplit/bin/nvcc --list-gpu-arch
         "3.5"
         "3.7"
         "5.0"
@@ -32,9 +32,8 @@ let
         "8.0"
         "8.6"
         "8.7"
-        ## the two compute capabilities below require newer nvcc (this environment uses CUDA 11.7)
-        # "8.9"
-        # "9.0"
+        "8.9"
+        "9.0"
     ];
 in {
     inherit filterAttrs;
--- dependencies/tiny-cuda-nn/default.nix
+++ dependencies/tiny-cuda-nn/default.nix
@@ -1,118 +1,117 @@
 # adapted from <https://github.com/NixOS/nixpkgs/blob/aacbb6d021d617881630c27088f311a5bc0e2145/pkgs/development/libraries/science/math/tiny-cuda-nn/default.nix>
 
-{ source, cudaCapabilities
-
-, buildSharedLib ? false
-
-, cmake
-, cudaPackages
-, lib
-, ninja
-, stdenv
-, symlinkJoin
-, which
-, fmt
+{ 
+    source, 
+    cudaCapabilities,
+    buildSharedLib ? false,
+    cmake,
+    cudaPackages,
+    lib,
+    ninja,
+    stdenv,
+    symlinkJoin,
+    which,
+    fmt,
 }:
 let
-  cuda-common-redist = with cudaPackages; [
-    libcublas # cublas_v2.h
-    libcusolver # cusolverDn.h
-    libcusparse # cusparse.h
-  ];
+    cuda-common-redist = with cudaPackages; [
+        libcublas # cublas_v2.h
+        libcusolver # cusolverDn.h
+        libcusparse # cusparse.h
+    ];
 
-  cuda-native-redist = symlinkJoin {
-    name = "cuda-redist";
-    paths = with cudaPackages; [
-      cuda_cudart # cuda_runtime.h
-      cuda_nvcc
-    ] ++ cuda-common-redist;
-  };
+    cuda-native-redist = symlinkJoin {
+        name = "cuda-redist";
+        paths = with cudaPackages; [
+            cuda_cudart # cuda_runtime.h
+            cuda_nvcc
+        ] ++ cuda-common-redist;
+    };
 
-  cuda-redist = symlinkJoin {
-    name = "cuda-redist";
-    paths = cuda-common-redist;
-  };
-in
-stdenv.mkDerivation (finalAttrs: rec {
-  pname = "tiny-cuda-nn";
-  version = "0.0.0";
+    cuda-redist = symlinkJoin {
+        name = "cuda-redist";
+        paths = cuda-common-redist;
+    };
+in stdenv.mkDerivation (finalAttrs: rec {
+    pname = "tiny-cuda-nn";
+    version = "0.0.0";
+    src = ./.;
+    outputs = [ "out" "dev" ];
 
-  src = ./.;
+    nativeBuildInputs = [
+        fmt
+        cmake
+        cuda-native-redist
+        ninja
+        which
+    ];
 
-  outputs = [ "out" "dev" ];
+    # build a shared library for faster development
+    postPatch = lib.optionalString buildSharedLib ''
+        sed -E \
+        -e 's/BUILD_SHARED_LIBS OFF/BUILD_SHARED_LIBS ON/g' \
+        -e 's/STATIC/SHARED/g' \
+        -i CMakeLists.txt
+    '';
 
-  nativeBuildInputs = [
-    fmt
-    cmake
-    cuda-native-redist
-    ninja
-    which
-  ];
+    # by default tcnn builds a static library, but that's too slow
+    cmakeFlags = [
+        "-DTCNN_BUILD_EXAMPLES=OFF"
+        "-DTCNN_BUILD_BENCHMARK=OFF"
+    ];
 
-  # build a shared library for faster development
-  postPatch = lib.optionalString buildSharedLib ''
-    sed -E \
-      -e 's/BUILD_SHARED_LIBS OFF/BUILD_SHARED_LIBS ON/g' \
-      -e 's/STATIC/SHARED/g' \
-      -i CMakeLists.txt
-  '';
+    buildInputs = [
+        cuda-redist
+    ];
 
-  # by default tcnn builds a static library, but that's too slow
-  cmakeFlags = [
-    "-DTCNN_BUILD_EXAMPLES=OFF"
-    "-DTCNN_BUILD_BENCHMARK=OFF"
-  ];
+    # NOTE: We cannot use pythonImportsCheck for this module because it uses torch to immediately
+    #       initialize CUDA and GPU access is not allowed in the nix build environment.
+    # NOTE: There are no tests for the C++ library or the python bindings, so we just skip the check
+    #   phase -- we're not missing anything.
+    doCheck = false;
 
-  buildInputs = [
-    cuda-redist
-  ];
+    preConfigure = let
+        dropDot = x: builtins.replaceStrings ["."] [""] x;
+    in ''
+        export TCNN_CUDA_ARCHITECTURES=${
+            lib.concatStringsSep "\\;" (map dropDot cudaCapabilities)
+        }
+        export CUDA_HOME=${cuda-native-redist}
+        export LIBRARY_PATH=${cuda-native-redist}/lib/stubs:$LIBRARY_PATH
+    '';
 
-  # NOTE: We cannot use pythonImportsCheck for this module because it uses torch to immediately
-  #       initialize CUDA and GPU access is not allowed in the nix build environment.
-  # NOTE: There are no tests for the C++ library or the python bindings, so we just skip the check
-  #   phase -- we're not missing anything.
-  doCheck = false;
+    installPhase = ''
+        runHook preInstall
 
-  preConfigure = let
-    dropDot = x: builtins.replaceStrings ["."] [""] x;
-  in ''
-    export TCNN_CUDA_ARCHITECTURES=${
-      lib.concatStringsSep "\\;" (map dropDot cudaCapabilities)
-    }
-    export CUDA_HOME=${cuda-native-redist}
-    export LIBRARY_PATH=${cuda-native-redist}/lib/stubs:$LIBRARY_PATH
-  '';
+        # install headers
+        mkdir -p $dev/include
+        cp -vr ../include/* $dev/include
+        cp -vr ../dependencies/* $dev/include
 
-  installPhase = ''
-    runHook preInstall
+        # install built library
+        mkdir -p $out/lib $out
+        cp -v libtiny-cuda-nn.${if buildSharedLib then "so" else "a"} $out/lib/
 
-    # install headers
-    mkdir -p $dev/include
-    cp -vr ../include/* $dev/include
-    cp -vr ../dependencies/* $dev/include
+        runHook postInstall
+    '';
+    # Fixes:
+    #   > RPATH of binary 
+    # /nix/store/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-tiny-cuda-nn-v1.6/lib/libtiny-cuda-nn.so 
+    # contains a forbidden reference to /build/
+    # REF: <https://github.com/calvertvl/nixpkgs/commit/6798060d8f5116a7d329df3265e82e0645aefc8f>
+    preFixup = lib.optionalString buildSharedLib ''
+        patchelf --set-rpath ${lib.makeLibraryPath buildInputs} $out/lib/libtiny-cuda-nn.so
+    '';
 
-    # install built library
-    mkdir -p $out/lib $out
-    cp -v libtiny-cuda-nn.${if buildSharedLib then "so" else "a"} $out/lib/
+    passthru = {
+        inherit cudaPackages;
+    };
 
-    runHook postInstall
-  '';
-  # Fixes:
-  #   > RPATH of binary /nix/store/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-tiny-cuda-nn-v1.6/lib/libtiny-cuda-nn.so contains a forbidden reference to /build/
-  # REF: <https://github.com/calvertvl/nixpkgs/commit/6798060d8f5116a7d329df3265e82e0645aefc8f>
-  preFixup = lib.optionalString buildSharedLib ''
-    patchelf --set-rpath ${lib.makeLibraryPath buildInputs} $out/lib/libtiny-cuda-nn.so
-  '';
-
-  passthru = {
-    inherit cudaPackages;
-  };
-
-  meta = with lib; {
-    description = "Lightning fast C++/CUDA neural network framework";
-    homepage = "https://github.com/NVlabs/tiny-cuda-nn";
-    license = licenses.bsd3;
-    maintainers = with maintainers; [ connorbaker ];
-    platforms = platforms.linux;
-  };
+    meta = with lib; {
+        description = "Lightning fast C++/CUDA neural network framework";
+        homepage = "https://github.com/NVlabs/tiny-cuda-nn";
+        license = licenses.bsd3;
+        maintainers = with maintainers; [ connorbaker ];
+        platforms = platforms.linux;
+    };
 })

01d405b27ad64be1ce20ccf81e593d3d0805e772
hayden-donnelly <hayden.git@proton.me>
2024-03-23 00:21:06 -0400

    Refactored march_rays_inference lowering rule.

--- dependencies/volume-rendering-jax/src/volrendjax/marching/lowering.py
+++ dependencies/volume-rendering-jax/src/volrendjax/marching/lowering.py
@@ -7,10 +7,9 @@ from volrendjax.lowering_helper import \
 
 def _march_rays_cuda_lowering_rule(
     ctx: mlir.LoweringRule,
-    rays_o: ir.Value, rays_d: ir.Value, t_starts: ir.Value, t_ends: ir.Value, 
-    noises: ir.Value, occupancy_bitfield: ir.Value,
-    total_samples: int, diagonal_n_steps: int, K: int, G: int, bound: float,
-    stepsize_portion: float,
+    rays_o: ir.Value, rays_d: ir.Value, t_starts: ir.Value, t_ends: ir.Value, noises: ir.Value, 
+    occupancy_bitfield: ir.Value,
+    total_samples: int, diagonal_n_steps: int, K: int, G: int, bound: float, stepsize_portion: float,
 ):
     n_rays, _ = ir.RankedTensorType(rays_o.type).shape
     opaque = volrendutils_cuda.make_marching_descriptor(
@@ -63,99 +62,60 @@ def _march_rays_cuda_lowering_rule(
     return out
 
 
-def march_rays_inference_lowering_rule(
+def _march_rays_inference_lowering_rule(
     ctx: mlir.LoweringRule,
-
-    # arrays
-    rays_o: ir.BlockArgument,
-    rays_d: ir.BlockArgument,
-    t_starts: ir.BlockArgument,
-    t_ends: ir.BlockArgument,
-    occupancy_bitfield: ir.BlockArgument,
-    next_ray_index_in: ir.BlockArgument,
-    terminated: ir.BlockArgument,
-    indices_in: ir.BlockArgument,
-
-    # static args
-    diagonal_n_steps: int,
-    K: int,
-    G: int,
-    march_steps_cap: int,
-    bound: float,
-    stepsize_portion: float,
+    rays_o: ir.BlockArgument, rays_d: ir.BlockArgument, t_starts: ir.BlockArgument, 
+    t_ends: ir.BlockArgument, occupancy_bitfield: ir.BlockArgument, next_ray_index_in: ir.BlockArgument, 
+    terminated: ir.BlockArgument, indices_in: ir.BlockArgument,
+    diagonal_n_steps: int, K: int, G: int, march_steps_cap: int, bound: float, stepsize_portion: float,
 ):
-    (n_total_rays, _), (n_rays,) = ir.RankedTensorType(rays_o.type).shape, ir.RankedTensorType(terminated.type).shape
-
+    n_total_rays, _ = ir.RankedTensorType(rays_o.type).shape
+    n_rays = ir.RankedTensorType(terminated.type).shape
     opaque = volrendutils_cuda.make_marching_inference_descriptor(
-        n_total_rays,
-        n_rays,
-        diagonal_n_steps,
-        K,
-        G,
-        march_steps_cap,
-        bound,
-        stepsize_portion,
+        n_total_rays, n_rays, diagonal_n_steps, K, G, march_steps_cap, bound, stepsize_portion,
     )
+    
+    rays_o_type, rays_o_shape = _get_ir_tensor_info(rays_o)
+    rays_d_type, rays_d_shape = _get_ir_tensor_info(rays_d)
+    t_starts_type, t_starts_shape = _get_ir_tensor_info(t_starts)
+    t_ends_type, t_ends_shape = _get_ir_tensor_info(t_ends)
+    occupancy_bitfield_type, occupancy_bitfield_shape = _get_ir_tensor_info(occupancy_bitfield)
+    next_ray_index_in_type, next_ray_index_in_shape = _get_ir_tensor_info(next_ray_index_in)
+    terminated_type, terminated_shape = _get_ir_tensor_info(terminated)
+    indices_in_type, indices_in_shape = _get_ir_tensor_info(indices_in)
 
-    shapes = {
-        "in.rays_o": (n_total_rays, 3),
-        "in.rays_d": (n_total_rays, 3),
-        "in.t_starts": (n_total_rays,),
-        "in.t_ends": (n_total_rays,),
-        "in.occupancy_bitfield": (K*G*G*G//8,),
-        "in.next_ray_index_in": (1,),
-        "in.terminated": (n_rays,),
-        "in.indices_in": (n_rays,),
-
-        "out.next_ray_index": (1,),
-        "out.indices_out": (n_rays,),
-        "out.n_samples": (n_rays,),
-        "out.t_starts": (n_rays,),
-        "out.xyzs": (n_rays, march_steps_cap, 3),
-        "out.dss": (n_rays, march_steps_cap),
-        "out.z_vals": (n_rays, march_steps_cap),
-    }
+    operands = [
+        rays_o, rays_d, t_starts, t_ends, occupancy_bitfield, 
+        next_ray_index_in, terminated, indices_in
+    ]
+    operand_shapes = [
+        rays_o_shape, rays_d_shape, t_starts_shape, t_ends_shape, occupancy_bitfield_shape,
+        next_ray_index_ind_shape, terminated_shape, indices_in_shape
+    ]
+    
+    next_ray_index_type, next_ray_index_shape = _make_ir_tensor_info((1,), 'uint32')
+    indices_out_type, indices_out_shape = _make_ir_tensor_info((n_rays,), 'uint32')
+    n_samples_type, n_samples_shape = _make_ir_tensor_info((n_rays,), 'uint32')
+    t_starts_out_type, t_starts_out_shape = _make_ir_tensor_info((n_rays,), 'fp32')
+    xyzs_type, xyzs_shape = _make_ir_tensor_info((n_rays, march_steps_cap, 3), 'fp32')
+    dss_type, dss_shape = _make_ir_tensor_info((n_rays, march_steps_cap), 'fp32')
+    z_vals_type, z_vals_shape = _make_ir_tensor_info((n_rays, march_steps_cap), 'fp32')
+    
+    result_types = [
+        next_ray_index_type, indices_out_type, n_samples_type, t_starts_out_type,
+        xyzs_type, dss_type, z_vals_type
+    ]
+    result_shapes = [
+        next_ray_index_shape, indices_out_shape, n_samples_shape, t_starts_out_shape,
+        xyzs_shape, dss_shape, z_vals_shape
+    ]
 
     out = custom_call(
         b'march_rays_inference',
-        result_types=[
-            ir.RankedTensorType.get(shapes["out.next_ray_index"], ir.IntegerType.get_unsigned(32)),
-            ir.RankedTensorType.get(shapes["out.indices_out"], ir.IntegerType.get_unsigned(32)),
-            ir.RankedTensorType.get(shapes["out.n_samples"], ir.IntegerType.get_unsigned(32)),
-            ir.RankedTensorType.get(shapes["out.t_starts"], ir.F32Type.get()),
-            ir.RankedTensorType.get(shapes["out.xyzs"], ir.F32Type.get()),
-            ir.RankedTensorType.get(shapes["out.dss"], ir.F32Type.get()),
-            ir.RankedTensorType.get(shapes["out.z_vals"], ir.F32Type.get()),
-        ],
-        operands=[
-            rays_o,
-            rays_d,
-            t_starts,
-            t_ends,
-            occupancy_bitfield,
-            next_ray_index_in,
-            terminated,
-            indices_in,
-        ],
+        result_types=result_types,
+        operands=operands,
         backend_config=opaque,
-        operand_layouts=_default_layouts(
-            shapes["in.rays_o"],
-            shapes["in.rays_d"],
-            shapes["in.t_starts"],
-            shapes["in.t_ends"],
-            shapes["in.occupancy_bitfield"],
-            shapes["in.next_ray_index_in"],
-            shapes["in.terminated"],
-            shapes["in.indices_in"],
-        ),
-        result_layouts=_default_layouts(
-            shapes["out.next_ray_index"],
-            shapes["out.indices_out"],
-            shapes["out.n_samples"],
-            shapes["out.t_starts"],
-            shapes["out.xyzs"],
-            shapes["out.dss"],
-            shapes["out.z_vals"],
-        ),
+        operand_layouts=_default_layouts(*operand_shapes),
+        result_layouts=_default_layouts(*result_shapes)
     ).results
     return out

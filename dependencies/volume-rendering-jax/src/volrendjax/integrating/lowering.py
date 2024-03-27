b5870c025c4f18914f1ca348ffc125fe1908c125
hayden-donnelly <hayden.git@proton.me>
2024-03-27 15:49:43 -0400

    Refactored volrendjax integration lowering rules.

--- dependencies/volume-rendering-jax/src/volrendjax/integrating/lowering.py
+++ dependencies/volume-rendering-jax/src/volrendjax/integrating/lowering.py
@@ -1,238 +1,145 @@
 from jax.interpreters import mlir
 from jax.interpreters.mlir import ir
 from jaxlib.hlo_helpers import custom_call
-from .. import volrendutils_cuda
-
-# helper function for mapping given shapes to their default mlir layouts
-def default_layouts(*shapes):
-    return [range(len(shape) - 1, -1, -1) for shape in shapes]
-
+from volrendjax import volrendutils_cuda
+from volrendjax.lowering_helper import \
+    _default_layouts, _get_ir_tensor_info, _make_ir_tensor_info
 
 def integrate_rays_lowering_rule(
     ctx: mlir.LoweringRuleContext,
-
-    rays_sample_startidx: ir.Value,
-    rays_n_samples: ir.Value,
-
-    bgs: ir.Value,
-    dss: ir.Value,
-    z_vals: ir.Value,
-    drgbs: ir.Value,
+    rays_sample_startidx: ir.Value, rays_n_samples: ir.Value, bgs: ir.Value, dss: ir.Value,
+    z_vals: ir.Value, drgbs: ir.Value,
 ):
-    n_rays, = ir.RankedTensorType(rays_sample_startidx.type).shape
-    total_samples, = ir.RankedTensorType(z_vals.type).shape
+    _, rays_sample_startidx_shape = _get_ir_tensor_info(rays_sample_startidx)
+    _, rays_n_samples_shape = _get_ir_tensor_info(rays_n_samples)
+    _, bgs_shape = _get_ir_tensor_info(bgs)
+    _, dss_shape = _get_ir_tensor_info(dss)
+    _, z_vals_shape = _get_ir_tensor_info(z_vals)
+    _, drgbs_shape = _get_ir_tensor_info(drgbs)
 
+    operands = [rays_sample_startidx, rays_n_samples, bgs, dss, z_vals, drgbs]
+    operand_shapes = [
+        rays_sample_startidx_shape, rays_n_samples_shape, 
+        bgs_shape, dss_shape, z_vals_shape, drgbs_shape
+    ]
+    
+    n_rays, = rays_sample_startidx_shape
+    total_samples, = z_vals_shape
     opaque = volrendutils_cuda.make_integrating_descriptor(n_rays, total_samples)
 
-    shapes = {
-        "in.rays_sample_startidx": (n_rays,),
-        "in.rays_n_samples": (n_rays,),
-
-        "in.bgs": (n_rays, 3),
-        "in.dss": (total_samples,),
-        "in.z_vals": (total_samples,),
-        "in.drgbs": (total_samples, 4),
-
-        "helper.measured_batch_size": (1,),
-
-        "out.final_rgbds": (n_rays, 4),
-        "out.final_opacities": (n_rays,),
-    }
-
+    measured_batch_size_type, measured_batch_size_shape = _make_ir_tensor_info((1,), 'uint32')
+    final_rgbds_type, final_rgbds_shape = _make_ir_tensor_info((n_rays, 4), 'fp32')
+    final_opacities_type, final_opacities_shape = _make_ir_tensor_info((n_rays,), 'fp32')
+    
+    result_types = [measured_batch_size_type, final_rgbds_type, final_opacities_type]
+    result_shapes = [measured_batch_size_shape, final_rgbds_shape, final_opacities_shape]
+    
     out = custom_call(
         call_target_name="integrate_rays",
-        result_types=[
-            ir.RankedTensorType.get(shapes["helper.measured_batch_size"], ir.IntegerType.get_unsigned(32)),
-            ir.RankedTensorType.get(shapes["out.final_rgbds"], ir.F32Type.get()),
-            ir.RankedTensorType.get(shapes["out.final_opacities"], ir.F32Type.get()),
-        ],
-        operands=[
-            rays_sample_startidx,
-            rays_n_samples,
-            bgs,
-            dss,
-            z_vals,
-            drgbs,
-        ],
+        result_types=result_types,
+        operands=operands,
         backend_config=opaque,
-        operand_layouts=default_layouts(
-            shapes["in.rays_sample_startidx"],
-            shapes["in.rays_n_samples"],
-            shapes["in.bgs"],
-            shapes["in.dss"],
-            shapes["in.z_vals"],
-            shapes["in.drgbs"],
-        ),
-        result_layouts=default_layouts(
-            shapes["helper.measured_batch_size"],
-            shapes["out.final_rgbds"],
-            shapes["out.final_opacities"],
-        ),
+        operand_layouts=_default_layouts(*operand_shapes),
+        result_layouts=_default_layouts(*result_shapes),
     ).results
     return out
-
 
 def integrate_rays_backward_lowring_rule(
     ctx: mlir.LoweringRuleContext,
-
-    rays_sample_startidx: ir.Value,
-    rays_n_samples: ir.Value,
-
-    # original inputs
-    bgs: ir.Value,
-    dss: ir.Value,
-    z_vals: ir.Value,
-    drgbs: ir.Value,
-
-    # original outputs
-    final_rgbds: ir.Value,
-    final_opacities: ir.Value,
-
-    # gradient inputs
+    rays_sample_startidx: ir.Value, rays_n_samples: ir.Value,
+    # Original inputs.
+    bgs: ir.Value, dss: ir.Value, z_vals: ir.Value, drgbs: ir.Value,
+    # Original outputs.
+    final_rgbds: ir.Value, final_opacities: ir.Value,
+    # Gradient inputs.
     dL_dfinal_rgbds: ir.Value,
-
-    # static argument
+    # Static argument.
     near_distance: float,
 ):
-    n_rays, = ir.RankedTensorType(rays_sample_startidx.type).shape
-    total_samples, = ir.RankedTensorType(z_vals.type).shape
+    _, rays_sample_startidx_shape = _get_ir_tensor_info(rays_sample_startidx)
+    _, rays_n_samples_shape = _get_ir_tensor_info(rays_n_samples)
+    _, bgs_shape = _get_ir_tensor_info(bgs)
+    _, dss_shape = _get_ir_tensor_info(dss)
+    _, z_vals_shape = _get_ir_tensor_info(z_vals)
+    _, drgbs_shape = _get_ir_tensor_info(drgbs)
+    _, final_rgbds_shape = _get_ir_tensor_info(final_rgbds)
+    _, final_opacities_shape = _get_ir_tensor_info(final_opacities)
+    _, dl_dfinal_rgbds_shape = _get_ir_tensor_info(dL_dfinal_rgbds)
 
-    opaque = volrendutils_cuda.make_integrating_backward_descriptor(n_rays, total_samples, near_distance)
+    operands = [
+        rays_sample_startidx, rays_n_samples, bgs, dss, z_vals, drgbs, final_rgbds,
+        final_opacities, dL_dfinal_rgbds,
+    ]
+    operand_shapes = [
+        rays_sample_startidx_shape, rays_n_samples_shape, bgs_shape, dss_shape, z_vals_shape,
+        drgbs_shape, final_rgbds_shape, final_opacities_shape, dl_dfinal_rgbds_shape
+    ]
 
-    shapes = {
-        "in.rays_sample_startidx": (n_rays,),
-        "in.rays_n_samples": (n_rays,),
+    n_rays, = rays_sample_startidx_shape
+    total_samples, = z_vals_shape
+    opaque = volrendutils_cuda.make_integrating_backward_descriptor(
+        n_rays, total_samples, near_distance
+    )
 
-        "in.bgs": (n_rays, 3),
-        "in.dss": (total_samples,),
-        "in.z_vals": (total_samples,),
-        "in.drgbs": (total_samples, 4),
+    dl_dbgs_type, dl_dbgs_shape = _make_ir_tensor_info((n_rays, 3), 'fp32')
+    dl_dz_vals_type, dl_dz_vals_shape = _make_ir_tensor_info((total_samples,), 'fp32')
+    dl_ddrgbs_type, dl_ddrgbs_shape = _make_ir_tensor_info((total_samples, 4), 'fp32')
 
-        "in.final_rgbds": (n_rays, 4),
-        "in.final_opacities": (n_rays,),
-
-        "in.dL_dfinal_rgbds": (n_rays, 4),
-
-        "out.dL_dbgs": (n_rays, 3),
-        "out.dL_dz_vals": (total_samples,),
-        "out.dL_ddrgbs": (total_samples, 4),
-    }
+    result_types = [dl_dbgs_type, dl_dz_vals_type, dl_ddrgbs_type]
+    result_shapes = [dl_dbgs_shape, dl_dz_vals_shape, dl_ddrgbs_shape]
 
     out = custom_call(
         call_target_name="integrate_rays_backward",
-        result_types=[
-            ir.RankedTensorType.get(shapes["out.dL_dbgs"], ir.F32Type.get()),
-            ir.RankedTensorType.get(shapes["out.dL_dz_vals"], ir.F32Type.get()),
-            ir.RankedTensorType.get(shapes["out.dL_ddrgbs"], ir.F32Type.get()),
-        ],
-        operands=[
-            rays_sample_startidx,
-            rays_n_samples,
-
-            bgs,
-            dss,
-            z_vals,
-            drgbs,
-
-            final_rgbds,
-            final_opacities,
-
-            dL_dfinal_rgbds,
-        ],
+        result_types=result_types,
+        operands=operands,
         backend_config=opaque,
-        operand_layouts=default_layouts(
-            shapes["in.rays_sample_startidx"],
-            shapes["in.rays_n_samples"],
-            shapes["in.bgs"],
-            shapes["in.dss"],
-            shapes["in.z_vals"],
-            shapes["in.drgbs"],
-
-            shapes["in.final_rgbds"],
-            shapes["in.final_opacities"],
-
-            shapes["in.dL_dfinal_rgbds"],
-        ),
-        result_layouts=default_layouts(
-            shapes["out.dL_dbgs"],
-            shapes["out.dL_dz_vals"],
-            shapes["out.dL_ddrgbs"],
-        ),
+        operand_layouts=_default_layouts(*operand_shapes),
+        result_layouts=_default_layouts(*result_shapes),
     ).results
     return out
 
-
 def integrate_rays_inference_lowering_rule(
     ctx: mlir.LoweringRuleContext,
-
-    rays_bg: ir.Value,
-    rays_rgbd: ir.Value,
-    rays_T: ir.Value,
-
-    n_samples: ir.Value,
-    indices: ir.Value,
-    dss: ir.Value,
-    z_vals: ir.Value,
-    drgbs: ir.Value,
+    rays_bg: ir.Value, rays_rgbd: ir.Value, rays_T: ir.Value, n_samples: ir.Value, 
+    indices: ir.Value, dss: ir.Value, z_vals: ir.Value, drgbs: ir.Value,
 ):
-    (n_total_rays, _) = ir.RankedTensorType(rays_rgbd.type).shape
-    (n_rays, march_steps_cap) = ir.RankedTensorType(dss.type).shape
+    _, rays_bg_shape = _get_ir_tensor_info(rays_bg)
+    _, rays_rgbd_shape = _get_ir_tensor_info(rays_rgbd)
+    _, rays_T_shape = _get_ir_tensor_info(rays_T)
+    _, n_samples_shape = _get_ir_tensor_info(n_samples)
+    _, indices_shape = _get_ir_tensor_info(indices)
+    _, dss_shape = _get_ir_tensor_info(dss)
+    _, z_vals_shape = _get_ir_tensor_info(z_vals)
+    _, drgbs_shape = _get_ir_tensor_info(drgbs)
 
-    opaque = volrendutils_cuda.make_integrating_inference_descriptor(n_total_rays, n_rays, march_steps_cap)
+    operands = [rays_bg, rays_rgbd, rays_T, n_samples, indices, dss, z_vals, drgbs]
+    operand_shapes = [
+        rays_bg_shape, rays_rgbd_shape, rays_T_shape, n_samples_shape, indices_shape,
+        dss_shape, z_vals_shape, drgbs_shape
+    ]
+    
+    n_total_rays, _ = rays_rgbd_shape
+    n_rays, march_steps_cap = dss_shape
+    opaque = volrendutils_cuda.make_integrating_inference_descriptor(
+        n_total_rays, n_rays, march_steps_cap
+    )
 
-    shapes = {
-        "in.rays_bg": (n_total_rays, 3),
-        "in.rays_rgbd": (n_total_rays, 4),
-        "in.rays_T": (n_total_rays,),
-
-        "in.n_samples": (n_rays,),
-        "in.indices": (n_rays,),
-        "in.dss": (n_rays, march_steps_cap),
-        "in.z_vals": (n_rays, march_steps_cap),
-        "in.drgbs": (n_rays, march_steps_cap, 4),
-
-        "out.terminate_cnt": (1,),
-        "out.terminated": (n_rays,),
-        "out.rays_rgbd": (n_rays, 4),
-        "out.rays_T": (n_rays,),
-    }
+    terminate_cnt_type, terminate_cnt_shape = _make_ir_tensor_info((1,), 'uint32')
+    terminated_type, terminated_shape = _make_ir_tensor_info((n_rays,), 'bool')
+    rays_rgbd_out_type, rays_rgbd_out_shape = _make_ir_tensor_info((n_rays, 4), 'fp32')
+    rays_T_out_type, rays_T_out_shape = _make_ir_tensor_info((n_rays,), 'fp32')
+    
+    result_types = [terminate_cnt_type, terminated_type, rays_rgbd_out_type, rays_T_out_type]
+    result_shapes = [
+        terminated_cnt_shape, terminated_shape, rays_rgbd_out_shape, rays_T_out_shape
+    ]
 
     out = custom_call(
         call_target_name="integrate_rays_inference",
-        result_types=[
-            ir.RankedTensorType.get(shapes["out.terminate_cnt"], ir.IntegerType.get_unsigned(32)),
-            ir.RankedTensorType.get(shapes["out.terminated"], ir.IntegerType.get_signless(1)),
-            ir.RankedTensorType.get(shapes["out.rays_rgbd"], ir.F32Type.get()),
-            ir.RankedTensorType.get(shapes["out.rays_T"], ir.F32Type.get()),
-        ],
-        operands=[
-            rays_bg,
-            rays_rgbd,
-            rays_T,
-
-            n_samples,
-            indices,
-            dss,
-            z_vals,
-            drgbs,
-        ],
+        result_types=result_types,
+        operands=operands,
         backend_config=opaque,
-        operand_layouts=default_layouts(
-            shapes["in.rays_bg"],
-            shapes["in.rays_rgbd"],
-            shapes["in.rays_T"],
-
-            shapes["in.n_samples"],
-            shapes["in.indices"],
-            shapes["in.dss"],
-            shapes["in.z_vals"],
-            shapes["in.drgbs"],
-        ),
-        result_layouts=default_layouts(
-            shapes["out.terminate_cnt"],
-            shapes["out.terminated"],
-            shapes["out.rays_rgbd"],
-            shapes["out.rays_T"],
-        ),
+        operand_layouts=_default_layouts(*operand_shapes),
+        result_layouts=_default_layouts(*result_shapes)
     ).results
     return out

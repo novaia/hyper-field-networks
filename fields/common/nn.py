e97d9d025b05870d049bd443b2ce2e90e9d6960f
hayden-donnelly <hayden.git@proton.me>
2024-03-28 19:42:17 -0400

    Moved 3d multi res hash encoding into separate function.

--- fields/common/nn.py
+++ fields/common/nn.py
@@ -2,8 +2,8 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 import math
+from functools import partial
 from typing import Callable, Any
-
 
 def hash_function_3d(x:jax.Array, table_size:int, hash_offset:jax.Array):
     pre_xor = x * jnp.array([1, 2654435761, 805459861])
@@ -13,10 +13,77 @@ def hash_function_3d(x:jax.Array, table_size:int, hash_offset:jax.Array):
     x = x + hash_offset
     return x
 
+def hash_function_2d(x:jax.Array, table_size:int, hash_offset:jax.Array):
+    pre_xor = x * jnp.array([1, 2654435761])
+    x = jnp.bitwise_xor(pre_xor[..., 0], pre_xor[..., 1])
+    x = x % table_size
+    x = x + hash_offset
+    return x
+
+def scale_point_to_hash_levels(x:jax.Array, scalings:jax.Array):
+    scaled = jnp.einsum('ij,k->ikj', x, scalings)
+    scaled_c = jnp.ceil(scaled).astype(jnp.int32)
+    scaled_f = jnp.floor(scaled).astype(jnp.int32)
+    point_offset = scaled - scaled_f
+    return scaled, scaled_c, scaled_f, point_offset
+
 def interpolate_hash_features(feature_a, feature_b, coefficient):
     def _scale(f, c):
         return jnp.einsum('ij,i->ij', f, c)
     return _scale(feature_a, coefficient) + _scale(feature_b, 1-coefficient)
+
+def multi_resolution_hash_encoding_3d(
+    x:jax.Array, scalings:jax.Array, hash_offset:jax.Array, table_size:int, hash_table:jax.Array
+):
+    scaled, scaled_c, scaled_f, point_offset = scale_point_to_hash_levels(x, scalings)
+    vertex_0 = scaled_c
+    vertex_1 = jnp.concatenate([scaled_c[..., 0:1], scaled_c[..., 1:2], scaled_f[..., 2:3]], axis=-1)
+    vertex_2 = jnp.concatenate([scaled_c[..., 0:1], scaled_f[..., 1:2], scaled_c[..., 2:3]], axis=-1)
+    vertex_3 = jnp.concatenate([scaled_f[..., 0:1], scaled_c[..., 1:2], scaled_c[..., 2:3]], axis=-1)
+    vertex_4 = jnp.concatenate([scaled_c[..., 0:1], scaled_f[..., 1:2], scaled_f[..., 2:3]], axis=-1)
+    vertex_5 = jnp.concatenate([scaled_f[..., 0:1], scaled_c[..., 1:2], scaled_f[..., 2:3]], axis=-1)
+    vertex_6 = jnp.concatenate([scaled_f[..., 0:1], scaled_f[..., 1:2], scaled_c[..., 2:3]], axis=-1)
+    vertex_7 = jnp.concatenate([scaled_f[..., 0:1], scaled_f[..., 1:2], scaled_f[..., 2:3]], axis=-1)
+    
+    # First vmap over batch dimension, then vmap over level dimension.
+    hash_fn = jax.vmap(
+        jax.vmap(
+            hash_function_3d, 
+            in_axes=(0, None, 0)
+        ), 
+        in_axes=(0, None, None)
+    )
+    hashed_0 = hash_fn(vertex_0, table_size, hash_offset)
+    hashed_1 = hash_fn(vertex_1, table_size, hash_offset)
+    hashed_2 = hash_fn(vertex_2, table_size, hash_offset)
+    hashed_3 = hash_fn(vertex_3, table_size, hash_offset)
+    hashed_4 = hash_fn(vertex_4, table_size, hash_offset)
+    hashed_5 = hash_fn(vertex_5, table_size, hash_offset)
+    hashed_6 = hash_fn(vertex_6, table_size, hash_offset)
+    hashed_7 = hash_fn(vertex_7, table_size, hash_offset)
+
+    f_0 = hash_table[hashed_0, :]
+    f_1 = hash_table[hashed_1, :]
+    f_2 = hash_table[hashed_2, :]
+    f_3 = hash_table[hashed_3, :]
+    f_4 = hash_table[hashed_4, :]
+    f_5 = hash_table[hashed_5, :]
+    f_6 = hash_table[hashed_6, :]
+    f_7 = hash_table[hashed_7, :]
+
+    # Linearly interpolate between all of the features.
+    interpolate_fn = jax.vmap(interpolate_hash_features, in_axes=0)
+    # First spatial dimension.
+    f_03 = interpolate_fn(f_0, f_3, point_offset[..., 0])
+    f_12 = interpolate_fn(f_1, f_2, point_offset[..., 0])
+    f_56 = interpolate_fn(f_5, f_6, point_offset[..., 0])
+    f_47 = interpolate_fn(f_4, f_7, point_offset[..., 0])
+    # Second spatial dimension.
+    f_0312 = interpolate_fn(f_03, f_12, point_offset[..., 1])
+    f_4756 = interpolate_fn(f_47, f_56, point_offset[..., 1])
+    # Third spatial dimension.
+    encoded = interpolate_fn(f_0312, f_4756, point_offset[..., 3])
+    return encoded
 
 class MultiResolutionHashEncoding(nn.Module):
     table_size: int
@@ -26,82 +93,27 @@ class MultiResolutionHashEncoding(nn.Module):
     feature_dim: int
 
     def setup(self):
-        self.levels = jnp.arange(self.num_levels)
-        self.hash_offset = self.levels * self.table_size
-        self.spatial_dim = 3
+        levels = jnp.arange(self.num_levels)
+        self.hash_offset = levels * self.table_size
         if self.num_levels > 1:
-            self.growth_factor = jnp.exp(
+            growth_factor = jnp.exp(
                 (jnp.log(self.max_resolution) - jnp.log(self.min_resolution)) 
                 / (self.num_levels - 1)
             )
         else:
-            self.growth_factor = 1
-        self.scalings = jnp.floor(self.min_resolution * self.growth_factor**self.levels)
-        #self.scalings = jnp.reshape(self.scalings, (self.scalings.shape[0], 1))
+            growth_factor = 1
+        self.scalings = jnp.floor(self.min_resolution * growth_factor**levels)
         absolute_table_size = self.table_size * self.num_levels
-        # Feature dim comes first so features can be broadcast with point offset.
-        # Feature shape is (feature_dim, num_levels). 
-        # Point offset shape is (spatial_dim, num_levels).
         self.hash_table = self.param(
             'hash_table', 
             nn.initializers.uniform(scale=10**-4), 
             (absolute_table_size, self.feature_dim)
         )
-
     
-    def __call__(self, x:jnp.ndarray):
-        scaled = jnp.einsum('ij,k->ikj', x, self.scalings)
-        scaled_c = jnp.ceil(scaled).astype(jnp.int32)
-        scaled_f = jnp.floor(scaled).astype(jnp.int32)
-        #point_offset = jnp.transpose(jnp.reshape(scaled - scaled_f, (self.spatial_dim, self.num_levels)))
-        point_offset = scaled - scaled_f
-        vertex_0 = scaled_c
-        vertex_1 = jnp.concatenate([scaled_c[..., 0:1], scaled_c[..., 1:2], scaled_f[..., 2:3]], axis=-1)
-        vertex_2 = jnp.concatenate([scaled_c[..., 0:1], scaled_f[..., 1:2], scaled_c[..., 2:3]], axis=-1)
-        vertex_3 = jnp.concatenate([scaled_f[..., 0:1], scaled_c[..., 1:2], scaled_c[..., 2:3]], axis=-1)
-        vertex_4 = jnp.concatenate([scaled_c[..., 0:1], scaled_f[..., 1:2], scaled_f[..., 2:3]], axis=-1)
-        vertex_5 = jnp.concatenate([scaled_f[..., 0:1], scaled_c[..., 1:2], scaled_f[..., 2:3]], axis=-1)
-        vertex_6 = jnp.concatenate([scaled_f[..., 0:1], scaled_f[..., 1:2], scaled_c[..., 2:3]], axis=-1)
-        vertex_7 = jnp.concatenate([scaled_f[..., 0:1], scaled_f[..., 1:2], scaled_f[..., 2:3]], axis=-1)
-        
-        # First vmap over batch dimension, then vmap over level dimension.
-        hash_fn = jax.vmap(
-            jax.vmap(
-                hash_function_3d, 
-                in_axes=(0, None, 0)
-            ), 
-            in_axes=(0, None, None)
+    def __call__(self, x:jax.Array):
+        encoded = multi_resolution_hash_encoding_3d(
+            x, self.scalings, self.hash_offset, self.table_size, self.hash_table
         )
-        hashed_0 = hash_fn(vertex_0, self.table_size, self.hash_offset)
-        hashed_1 = hash_fn(vertex_1, self.table_size, self.hash_offset)
-        hashed_2 = hash_fn(vertex_2, self.table_size, self.hash_offset)
-        hashed_3 = hash_fn(vertex_3, self.table_size, self.hash_offset)
-        hashed_4 = hash_fn(vertex_4, self.table_size, self.hash_offset)
-        hashed_5 = hash_fn(vertex_5, self.table_size, self.hash_offset)
-        hashed_6 = hash_fn(vertex_6, self.table_size, self.hash_offset)
-        hashed_7 = hash_fn(vertex_7, self.table_size, self.hash_offset)
-
-        f_0 = self.hash_table[hashed_0, :]
-        f_1 = self.hash_table[hashed_1, :]
-        f_2 = self.hash_table[hashed_2, :]
-        f_3 = self.hash_table[hashed_3, :]
-        f_4 = self.hash_table[hashed_4, :]
-        f_5 = self.hash_table[hashed_5, :]
-        f_6 = self.hash_table[hashed_6, :]
-        f_7 = self.hash_table[hashed_7, :]
-
-        # Linearly interpolate between all of the features.
-        interpolate_fn = jax.vmap(interpolate_hash_features, in_axes=0)
-        # First spatial dimension.
-        f_03 = interpolate_fn(f_0, f_3, point_offset[..., 0])
-        f_12 = interpolate_fn(f_1, f_2, point_offset[..., 0])
-        f_56 = interpolate_fn(f_5, f_6, point_offset[..., 0])
-        f_47 = interpolate_fn(f_4, f_7, point_offset[..., 0])
-        # Second spatial dimension.
-        f_0312 = interpolate_fn(f_03, f_12, point_offset[..., 1])
-        f_4756 = interpolate_fn(f_47, f_56, point_offset[..., 1])
-        # Third spatial dimension.
-        encoded = interpolate_fn(f_0312, f_4756, point_offset[..., 3])
         return jnp.reshape(encoded, (encoded.shape[0], self.num_levels * self.feature_dim))
 
 class MultiResolutionHashEncoding2D(nn.Module):
@@ -123,27 +135,15 @@ class MultiResolutionHashEncoding2D(nn.Module):
         else:
             self.growth_factor = 1
         self.scalings = jnp.floor(self.min_resolution * self.growth_factor**self.levels)
-        self.scalings = jnp.reshape(self.scalings, (self.scalings.shape[0], 1))
         absolute_table_size = self.table_size * self.num_levels
-        # Feature dim comes first so features can be broadcast with point offset.
-        # Feature shape is (feature_dim, num_levels). 
-        # Point offset shape is (spatial_dim, num_levels).
         self.hash_table = self.param(
             'hash_table', 
             nn.initializers.uniform(scale=10**-4), 
-            (self.feature_dim, absolute_table_size)
+            (absolute_table_size, self.feature_dim)
         )
 
-    def hash_function(self, x:jnp.ndarray, table_size:int, hash_offset:jnp.ndarray):
-        x = jnp.repeat(jnp.expand_dims(x, axis=-1), repeats=2, axis=-1)
-        pre_xor = x * jnp.array([1, 2654435761])
-        x = jnp.bitwise_xor(pre_xor[..., 0:1], pre_xor[..., 1:2])
-        x = x % table_size
-        x = x + hash_offset
-        return x
-    
     def __call__(self, x:jnp.ndarray):
-        scaled = x * self.scalings
+        scaled = jnp.einsum('ij,k->ikj', x, self.scalings)
         scaled_c = jnp.ceil(scaled).astype(jnp.int32)
         scaled_f = jnp.floor(scaled).astype(jnp.int32)
         point_offset = jnp.reshape(scaled - scaled_f, (self.spatial_dim, self.num_levels))

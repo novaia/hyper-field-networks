3a21a479b5bdf8493440700a47ee707a7bcb21ce
hayden-donnelly <hayden.git@proton.me>
2024-04-02 18:46:11 -0400

    Added function to compute model matrix.

--- prototyping/gl_rendering/src/transform.c
+++ prototyping/gl_rendering/src/transform.c
@@ -3,14 +3,32 @@
 #include <math.h>
 #include "transform.h"
 
-static double degrees_to_radians(double degrees)
+static inline double degrees_to_radians(double degrees)
 {
     const double pi = 3.14159265358979323846;
     return degrees * (pi / 180.0);
 }
 
-mat4 get_perspective_matrix(float fov, float near_plane, float far_plane, float aspect_ratio)
+inline mat4 mat4mul(mat4 a, mat4 b)
 {
+    mat4 result = { .data = {0.0f} };
+    for(unsigned int col = 0; col < 4; col++)
+    {
+        const unsigned int col_offset = col * 4;
+        for(unsigned int row = 0; row < 4; row++)
+        {
+            for(unsigned int i = 0; i < 4; i++)
+            {
+                result.data[col_offset + row] += a.data[i * 4 + row] * b.data[col_offset + i];
+            }
+        }
+    }
+    return result;
+}
+
+inline mat4 get_perspective_matrix(
+    float fov, float near_plane, float far_plane, float aspect_ratio
+){
     const float tan_half_fov = (float)tan(degrees_to_radians((double)fov) / 2.0);
     mat4 perspective_matrix = { 
         .data = {
@@ -23,7 +41,7 @@ mat4 get_perspective_matrix(float fov, float near_plane, float far_plane, float 
     return perspective_matrix;
 }
 
-mat4 get_x_rotation_matrix(float angle)
+inline mat4 get_x_rotation_matrix(float angle)
 {
     const float theta = (float)degrees_to_radians((double)angle);
     const float cos_theta = (float)cos(theta);
@@ -39,7 +57,7 @@ mat4 get_x_rotation_matrix(float angle)
     return x_rotation_matrix;
 }
 
-mat4 get_y_rotation_matrix(float angle)
+inline mat4 get_y_rotation_matrix(float angle)
 {
     const float theta = (float)degrees_to_radians((double)angle);
     const float cos_theta = (float)cos(theta);
@@ -53,4 +71,17 @@ mat4 get_y_rotation_matrix(float angle)
         }
     };
     return y_rotation_matrix;
+}
+
+inline mat4 get_model_matrix(
+    float position_x, float position_y, float position_z, 
+    float rotation_x, float rotation_y, float rotation_z
+){
+    const mat4 x_rotation_matrix = get_x_rotation_matrix(rotation_x);
+    const mat4 y_rotation_matrix = get_y_rotation_matrix(rotation_y);
+    mat4 model_matrix = mat4mul(x_rotation_matrix, y_rotation_matrix);
+    model_matrix[MAT4_X_TRANSLATION_INDEX] = position_x;
+    model_matrix[MAT4_Y_TRANSLATION_INDEX] = position_y;
+    model_matrix[MAT4_Z_TRANSLATION_INDEX] = position_z;
+    return model_matrix;
 }
--- prototyping/gl_rendering/src/transform.h
+++ prototyping/gl_rendering/src/transform.h
@@ -4,14 +4,25 @@
 extern "C" {
 #endif
 
+#define MAT4_X_TRANSLATION_INDEX 12
+#define MAT4_Y_TRANSLATION_INDEX 13
+#define MAT4_Z_TRANSLATION_INDEX 14
+
 typedef struct
 {
     float data[16];
 } mat4;
 
-mat4 get_perspective_matrix(float fov, float near_plane, float far_plane, float aspect_ratio);
-mat4 get_x_rotation_matrix(float angle);
-mat4 get_y_rotation_matrix(float angle);
+inline mat4 mat4mul(mat4 a, mat4 b);
+inline mat4 get_perspective_matrix(
+    float fov, float near_plane, float far_plane, float aspect_ratio
+);
+inline mat4 get_x_rotation_matrix(float angle);
+inline mat4 get_y_rotation_matrix(float angle);
+inline mat4 get_model_matrix(
+    float position_x, float position_y, float position_z, 
+    float rotation_x, float rotation_y, float rotation_z
+);
 
 #ifdef __cplusplus
 } // extern "C"

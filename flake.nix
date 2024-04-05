b68338ceb06887e7f2d6d81ebb99b841f1772f2c
hayden-donnelly <hayden.git@proton.me>
2024-04-05 14:22:21 -0400

    Added nixGL to flake and removed manual cuda/nvidia driver path sets.

--- flake.nix
+++ flake.nix
@@ -5,16 +5,23 @@
         nixpkgs.url = "github:nixos/nixpkgs/23.11";
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
         flake-utils.url = "github:numtide/flake-utils";
+        # Patched version of nixGL from kenrandunderscore.
+        # PR: https://github.com/nix-community/nixGL/pull/165
+        # TODO: switch back to github:nix-community/nixGL when PR is merged.
+        nixgl.url = "github:hayden-donnelly/nixGL";
     };
-    outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-utils, ... }: 
+    outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-utils, nixgl, ... }: 
     let 
         dependencies = import ./dependencies;
-    in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: {
+    in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+        inherit (nixpkgs-unstable) lib;
+    in {
         devShells = let
             pyVer = "311";
             py = "python${pyVer}";
             overlays = [
                 dependencies.overlay
+                nixgl.overlay
                 (final: prev: {
                     ${py} = prev.${py}.override {
                         packageOverrides = finalPkgs: prevPkgs: {
@@ -75,8 +82,8 @@
                 ];
                 shellHook = ''
                     export CUDA_PATH=${unstableCudaPkgs.cudatoolkit}
-                    export EXTRA_LDFLAGS="-L/lib -L${unstableCudaPkgs.linuxPackages.nvidia_x11}/lib"
-                    export LD_LIBRARY_PATH=/run/opengl-driver/lib/
+                    source <(sed -Ee '/\$@/d' ${lib.getExe unstableCudaPkgs.nixgl.nixGLIntel})
+                    source <(sed -Ee '/\$@/d' ${lib.getExe unstableCudaPkgs.nixgl.auto.nixGLNvidia}*)
                 '';
             };
         };

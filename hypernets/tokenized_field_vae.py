608e5e201082372e1b1e1815dcf048b85fc75f56
hayden-donnelly <hayden.git@proton.me>
2024-04-08 17:08:13 -0400

    Implemented tokenized field vae model architecture.
    The point of this is to deal with the multiple scales of various neural
    field parameters by tokenizing them and then mapping them into a
    contious latent space. This should also improve the efficiency of
    downstream hyper diffusion models since they will be latent space
    diffusion instead of weight space diffusion.

--- hypernets/tokenized_field_vae.py
+++ hypernets/tokenized_field_vae.py
@@ -1,7 +1,9 @@
 import jax
 from jax import numpy as jnp
+from flax import linen as nn
 import datasets
 import os, json
+from typing import Any
 
 def load_dataset(dataset_path):
     field_config = None
@@ -31,12 +33,75 @@ def load_dataset(dataset_path):
     dataset = dataset.with_format('jax')
     return dataset, field_config, param_map
 
+class TokenizedFieldVae(nn.Module):
+    vocab_size: int
+    context_length: int
+    embedding_dim: int
+    latent_dim: int
+    num_attention_heads: int
+    num_blocks: int
+    dtype: Any = jnp.float32
+
+    @nn.compact
+    def __call__(self, tokens, key):
+        def transformer_block(x):
+            for _ in range(self.num_blocks):
+                residual = x
+                x = nn.LayerNorm(dtype=self.dtype)(x)
+                x = nn.MultiHeadDotProductAttention(
+                    num_heads=self.num_attention_heads,
+                    qkv_features=self.embedding_dim,
+                    out_features=self.embedding_dim,
+                    dtype=self.dtype
+                )(inputs_q=x)
+                x = x + residual
+                residual = x
+                x = nn.LayerNorm(dtype=self.dtype)(x)
+                x = nn.Dense(features=self.embedding_dim, dtype=self.dtype)(x)
+                x = nn.gelu(x)
+                x = nn.Dense(features=self.embedding_dim, dtype=self.dtype)(x)
+                x = nn.gelu(x)
+                x = x + residual
+                return x
+
+        x = nn.Embed(num_embeddings=self.vocab_size, features=self.embedding_dim)(tokens)
+        x = transformer_block(x)
+        means = nn.DenseGeneral(features=self.latent_dim, axis=(-1, -2), dtype=self.dtype)(x)
+        logvars = nn.DenseGeneral(features=self.latent_dim, axis=(-1, -2), dtype=self.dtype)(x)
+        x = means + jnp.exp(0.5 * logvars) * jax.random.normal(key, means.shape)
+        x = nn.DenseGeneral(features=(self.context_length, self.embedding_dim), axis=-1, dtype=self.dtype)(x)
+        x = transformer_block(x)
+        logits = nn.Dense(features=self.vocab_size)(x)
+        return logits
+
 def main():
     dataset_path = 'data/mnist-ngp-image-612-11bit'
     dataset, field_config, param_map = load_dataset(dataset_path)
-    print(len(dataset))
-    print(field_config)
-    print(param_map)
+    
+    token_bits = 11
+    vocab_size = 2 * 2**token_bits - 1
+    print('vocab size', vocab_size)
+
+    batch_size = 16
+    context_length = 612
+    embedding_dim = 64
+    latent_dim = 512
+    num_attention_heads = 4
+    num_blocks = 8
+    
+    model = TokenizedFieldVae(
+        vocab_size=vocab_size,
+        context_length=context_length,
+        embedding_dim=embedding_dim,
+        latent_dim=latent_dim,
+        num_attention_heads=num_attention_heads,
+        num_blocks=num_blocks,
+        dtype=jnp.bfloat16
+    )
+    x = jnp.ones((batch_size, context_length), dtype=jnp.uint32)
+    vae_key = jax.random.PRNGKey(68)
+    params_key = jax.random.PRNGKey(91)
+    params = model.init(params_key, x, vae_key)['params']
 
 if __name__ == '__main__':
     main()

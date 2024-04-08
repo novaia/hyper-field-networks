23d67b9e180a95e60c000e530d70d20246f5db2a
hayden-donnelly <hayden.git@proton.me>
2024-04-07 21:36:18 -0400

    Added fp tokenization and detokenization to render validation script.

--- scripts/render_if_from_pq.py
+++ scripts/render_if_from_pq.py
@@ -1,11 +1,13 @@
 import json
 import jax
+from jax import numpy as jnp
 from datasets import load_dataset
 from fields.ngp_image import (
     create_model_from_config, create_train_state, render_image
 )
 from fields.common.flattening import unflatten_params
 from matplotlib import pyplot as plt
+from fp_conversion import tokenize, detokenize
 
 with open('configs/colored_monsters_ngp_image.json', 'r') as f:
     config = json.load(f)
@@ -14,8 +16,14 @@ with open('data/something3/param_map.json', 'r') as f:
 
 dataset = load_dataset('parquet', data_files={'train': 'data/output.parquet'}, split='train')
 dataset = dataset.with_format('jax')
-flat_params = dataset[0]['params']
+flat_params = dataset[40]['params']
+flat_params = jnp.array(flat_params, dtype=jnp.float16)
 print('flat params shape', flat_params.shape)
+
+param_tokens = tokenize(flat_params)
+print(param_tokens[:20])
+flat_params = detokenize(param_tokens)
+
 params = unflatten_params(flat_params, param_map)
 
 model = create_model_from_config(config)

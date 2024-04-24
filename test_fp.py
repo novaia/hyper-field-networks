b94422d075733346b1b1de7748be4d5b763ca236
hayden-donnelly <hayden.git@proton.me>
2024-04-24 01:05:59 -0400

    Invalid tokens seem to appear when the number of params increases.
    This is probably why the mnist tokens were all correct while the
    colored-monsters tokens are not. The former only has 612 params while
    the latter has about 17k.

--- test_fp.py
+++ test_fp.py
@@ -1,7 +1,10 @@
 from float_tokenization import tokenize, detokenize 
 from jax import numpy as jnp
+import jax
 
-tokens = tokenize(jnp.arange(20, dtype=jnp.float16))
+samples_in = jax.random.normal(jax.random.PRNGKey(0), (15000,), dtype=jnp.float16)
+print(samples_in)
+tokens = tokenize(samples_in)
 print(tokens)
 samples = detokenize(tokens)
 print(samples)

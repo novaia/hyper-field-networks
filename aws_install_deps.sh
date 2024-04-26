1ad72e42450e729bd72c3ef59c39440fa06fe193
hayden-donnelly <hayden.git@proton.me>
2024-04-25 22:08:30 -0400

    Deleted aws_install_deps scripts.
    Nix can be used to install dependencies in cloud environments.

--- aws_install_deps.sh
+++
@@ -1,9 +0,0 @@
-JAX_PACKAGE_URL="https://storage.googleapis.com/jax-releases/jax_cuda_releases.html"
-python3 -m pip install -U "jax[cuda12_pip]" -f $JAX_PACKAGE_URL
-
-DALI_PACKAGE_URL="https://developer.download.nvidia.com/compute/redist"
-python3 -m pip install -y \
-    --extra-index-url $DALI_PACKAGE_URL \
-    --upgrade nvidia-dali-cuda120
-
-python3 -m pip install -r requirements.txt

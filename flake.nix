6c972a1678a0d56195a2a449924162748d9f2c57
hayden-donnelly <hayden.git@proton.me>
2024-06-10 23:12:01 -0400

    Removed gcc12, cudatoolkit, cudnn, and cudart from devshell.
    Also cleaned up formatting of flake.
    
    Not sure if removing these will affect jax's ability to access cuda in
    anyway. I tested this new setup by printing out the gpu from jax
    (jax.devices('gpu')[0]) and jax had no problems finding it, but I'll
    have try running a full training script once the gpu is no longer
    occupied. These dependencies were quite large which is why I removed
    them.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
     description = "3D generative AI";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/23.11";
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
         flake-utils.url = "github:numtide/flake-utils";
         # Patched version of nixGL from kenrandunderscore.
@@ -10,82 +9,84 @@
         # TODO: switch back to github:nix-community/nixGL when PR is merged.
         nixgl.url = "github:hayden-donnelly/nixGL";
     };
-    outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, flake-utils, nixgl, ... }: 
-    let 
-        dependencies = import ./dependencies;
-    in flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
-        inherit (nixpkgs-unstable) lib;
-    in {
-        devShells = let
-            pyVer = "311";
-            py = "python${pyVer}";
-            overlays = [
-                dependencies.overlay
-                nixgl.overlay
-                (final: prev: {
-                    ${py} = prev.${py}.override {
-                        packageOverrides = finalPkgs: prevPkgs: {
-                            jax = prevPkgs.jax.overridePythonAttrs (o: {
-                                nativeCheckInputs = [];
-                                pythonImportsCheck = [];
-                                pytestFlagsArray = [];
-                                passthru.tests = [];
-                                doCheck = false;
-                            });
-                            # For some reason Flax has jaxlib as a buildInput and tensorflow as a 
-                            # nativeCheckInput, so set jaxlib to jaxlib-bin in order to avoid building 
-                            # jaxlib and turn off all checks to avoid building tensorflow.
-                            jaxlib = prevPkgs.jaxlib-bin;
-                            flax = prevPkgs.flax.overridePythonAttrs (o: {
-                                nativeCheckInputs = [];
-                                pythonImportsCheck = [];
-                                pytestFlagsArray = [];
-                                doCheck = false;
-                            });
-                            wandb = prevPkgs.wandb.overridePythonAttrs(o: {
-                                nativeCheckInputs = [];
-                                pythonIMportsCheck = [];
-                                doCheck = false;
-                            });
+    outputs = inputs@{ 
+        self, 
+        nixpkgs-unstable, 
+        flake-utils, 
+        nixgl, 
+        ... 
+    }: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: 
+        let
+            inherit (nixpkgs-unstable) lib;
+            dependencies = import ./dependencies;
+        in {
+            devShells = let
+                pyVer = "311";
+                py = "python${pyVer}";
+                overlays = [
+                    dependencies.overlay
+                    nixgl.overlay
+                    (final: prev: {
+                        ${py} = prev.${py}.override {
+                            packageOverrides = finalPkgs: prevPkgs: {
+                                jax = prevPkgs.jax.overridePythonAttrs (o: {
+                                    nativeCheckInputs = [];
+                                    pythonImportsCheck = [];
+                                    pytestFlagsArray = [];
+                                    passthru.tests = [];
+                                    doCheck = false;
+                                });
+                                # For some reason Flax has jaxlib as a buildInput and tensorflow as a 
+                                # nativeCheckInput, so set jaxlib to jaxlib-bin in order to avoid building 
+                                # jaxlib and turn off all checks to avoid building tensorflow.
+                                jaxlib = prevPkgs.jaxlib-bin;
+                                flax = prevPkgs.flax.overridePythonAttrs (o: {
+                                    nativeCheckInputs = [];
+                                    pythonImportsCheck = [];
+                                    pytestFlagsArray = [];
+                                    doCheck = false;
+                                });
+                                wandb = prevPkgs.wandb.overridePythonAttrs(o: {
+                                    nativeCheckInputs = [];
+                                    pythonIMportsCheck = [];
+                                    doCheck = false;
+                                });
+                            };
                         };
+                    })
+                ];
+                pkgs = import nixpkgs-unstable {
+                    inherit system overlays;
+                    config = {
+                        allowUnfree = true;
+                        cudaSupport = true;
                     };
-                })
-            ];
-            unstableCudaPkgs = import nixpkgs-unstable {
-                inherit system overlays;
-                config = {
-                    allowUnfree = true;
-                    cudaSupport = true;
+                };
+            in rec {
+                default = pkgs.mkShell {
+                    name = "cuda";
+                    buildInputs = with pkgs; [
+                        (pkgs.${py}.withPackages (pyp: with pyp; [
+                            jax
+                            jaxlib-bin
+                            flax
+                            pillow
+                            pyarrow
+                            datasets
+                            wandb
+                            matplotlib
+                            pytest
+                            ngp-volume-rendering
+                            serde-helper
+                        ]))
+                    ];
+                    shellHook = ''
+                        export CUDA_PATH=${pkgs.cudatoolkit}
+                        source <(sed -Ee '/\$@/d' ${lib.getExe pkgs.nixgl.nixGLIntel})
+                        source <(sed -Ee '/\$@/d' ${lib.getExe pkgs.nixgl.auto.nixGLNvidia}*)
+                    '';
                 };
             };
-        in rec {
-            default = unstableCudaPkgs.mkShell {
-                name = "cuda";
-                buildInputs = [
-                    (unstableCudaPkgs.${py}.withPackages (pyp: with pyp; [
-                        jax
-                        jaxlib-bin
-                        flax
-                        pillow
-                        pyarrow
-                        datasets
-                        wandb
-                        matplotlib
-                        pytest
-                        unstableCudaPkgs.ngp-volume-rendering
-                        unstableCudaPkgs.serde-helper
-                    ]))
-                    unstableCudaPkgs.gcc12
-                    unstableCudaPkgs.cudaPackages.cudatoolkit
-                    unstableCudaPkgs.cudaPackages.cuda_cudart
-                    unstableCudaPkgs.cudaPackages.cudnn
-                ];
-                shellHook = ''
-                    export CUDA_PATH=${unstableCudaPkgs.cudatoolkit}
-                    source <(sed -Ee '/\$@/d' ${lib.getExe unstableCudaPkgs.nixgl.nixGLIntel})
-                    source <(sed -Ee '/\$@/d' ${lib.getExe unstableCudaPkgs.nixgl.auto.nixGLNvidia}*)
-                '';
-            };
-        };
-    });
+        }
+    );
 }

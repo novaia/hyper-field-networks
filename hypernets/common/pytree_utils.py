f996681981388f643e83c74327b20b760efc6e78
hayden-donnelly <hayden.git@proton.me>
2024-06-17 17:01:03 -0400

    Finished script that encodes and visualizes hypernet latents.

--- hypernets/common/pytree_utils.py
+++ hypernets/common/pytree_utils.py
@@ -1,10 +1,18 @@
 import jax
+import jax.numpy as jnp
 import numpy as np
 
-def move_pytree_to_cpu(pytree):
+def move_pytree_to_cpu(pytree, cpu_id=0):
+    device = jax.devices('cpu')[0]
     def move_to_cpu(tensor):
-        return np.array(jax.device_put(tensor, device=jax.devices('cpu')[0]))
+        return np.array(jax.device_put(tensor, device=device))
     return jax.tree_map(move_to_cpu, pytree)
+
+def move_pytree_to_gpu(pytree, gpu_id=0):
+    device = jax.devices('gpu')[0]
+    def move_to_gpu(tensor):
+        return jax.device_put(jnp.array(tensor), device=device)
+    return jax.tree_map(move_to_gpu, pytree)
 
 # Reference: https://gist.github.com/Narsil/d5b0d747e5c8c299eb6d82709e480e3d
 def flatten_dict(weights, prefix=""):
--- hypernets/util_scripts/encode_and_visualize.py
+++ hypernets/util_scripts/encode_and_visualize.py
@@ -1,27 +1,53 @@
 import os, json, argparse
-import safetensors
+from safetensors.flax import load_file
 from hypernets.split_field_conv_ae import (
-    SplitFieldConvAeConfig, init_model_from_config
+    SplitFieldConvAeConfig, init_model_from_config, preprocess
 )
 import numpy as np
+from flax import traverse_util
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+
+from hypernets.common.pytree_utils import move_pytree_to_gpu
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, required=True)
-    parser.add_argument('--params', type=str, required=True)
+    parser.add_argument('--encoder', type=str, required=True)
     parser.add_argument('--sample', type=str, required=True)
     args = parser.parse_args()
 
     with open(args.config, 'r') as f:
         model_config = SplitFieldConvAeConfig(json.load(f))
     _, encoder_model, _ = init_model_from_config(model_config)
-    params = safetensors.flax.load_file(args.params)
-    sample = np.load(args.sample, allow_pickle=False)
+    
+    sample_cpu = np.load(args.sample, allow_pickle=False)
+    sample = jax.device_put(jnp.array(sample_cpu), device=jax.devices('gpu')[0])
+    params_cpu = traverse_util.unflatten_dict(load_file(args.encoder), sep='.')
+    params = move_pytree_to_gpu(params_cpu)
+    
+    sample = jnp.expand_dims(sample, axis=0)
+    sample = preprocess(
+        x=sample,
+        train_on_hash_grid=model_config.train_on_hash_grid,
+        hash_grid_end=model_config.num_hash_grid_params,
+        left_padding=model_config.left_padding,
+        right_padding=model_config.right_padding,
+        requires_padding=model_config.requires_padding
+    )
+    latent_gpu = encoder_model.apply({'params': params}, x=sample)[0]
+    latent = np.array(jax.device_put(latent_gpu, device=jax.devices('cpu')[0]), dtype=np.float32)
+    latent = np.ravel(latent)
 
-    # TODO:
-    # - unflatten params
-    # - call encoder_model.apply on sample
-    # - save output to png
+    indices = np.arange(latent.shape[0])
+    plt.figure(figsize=(10, 6))
+    plt.scatter(indices, latent, s=10)
+    plt.xlabel('Index')
+    plt.ylabel('Latent Value')
+    plt.title('Scatter Plot of Latent Values')
+    plt.grid(True)
+    plt.show()
 
 if __name__ == '__main__':
     main()

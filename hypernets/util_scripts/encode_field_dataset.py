0e2f92a47d6bf6018374c20b1219fd7c72fd2b3c
hayden-donnelly <hayden.git@proton.me>
2024-07-05 17:08:22 -0400

    Switched to batch processing for encoding fields into latents.

--- hypernets/util_scripts/encode_field_dataset.py
+++ hypernets/util_scripts/encode_field_dataset.py
@@ -70,47 +70,55 @@ def main():
     params = move_pytree_to_gpu(params_cpu)
 
     dataset = get_dataset(args.input_dataset)
-    batch_size = 1
-    dataset_iterator = dataset.iter(batch_size=32)
+    batch_size = 32
+    dataset_iterator = dataset.iter(batch_size=batch_size)
     num_samples = len(dataset)
+    num_batches = num_samples // batch_size 
     print('Samples in dataset:', num_samples)
+    print('Batches in dataset:', num_batches)
 
     samples_per_table = 30000
     current_table_index = 0
     pq_table_data = []
 
-    for i, sample in enumerate(dataset_iterator):
-        params_sample = sample['params'][0]
-        params_sample = jax.device_put(jnp.array(params_sample), device=jax.devices('gpu')[0])
+    for batch_id in range(num_batches):
+        batch_samples = next(dataset_iterator)['params']
+        batch_samples = jax.device_put(jnp.array(batch_samples), device=jax.devices('gpu')[0])
         
-        params_sample = jnp.expand_dims(params_sample, axis=0)
-        params_sample = preprocess(
-            x=params_sample,
+        batch_samples = preprocess(
+            x=batch_samples,
             train_on_hash_grid=model_config.train_on_hash_grid,
             hash_grid_end=model_config.num_hash_grid_params,
             left_padding=model_config.left_padding,
             right_padding=model_config.right_padding,
             requires_padding=model_config.requires_padding
         )
-        latent_gpu = encoder_model.apply({'params': params}, x=params_sample)[0]
-        latent = np.array(jax.device_put(latent_gpu, device=jax.devices('cpu')[0]), dtype=np.float32)
-        latent = np.ravel(latent)
+        batch_latents_gpu = encoder_model.apply({'params': params}, x=batch_samples)[0]
+        batch_latents = np.array(
+            jax.device_put(batch_latents_gpu, device=jax.devices('cpu')[0]), 
+            dtype=np.float32
+        )
+        for latent_id in range(batch_size):
+            latent = np.ravel(batch_latents[latent_id])
 
-        pq_row_data = {
-            'latents': latent.tolist(),
-        }
-        pq_table_data.append(pq_row_data)
+            pq_row_data = {
+                'latents': latent.tolist(),
+            }
+            pq_table_data.append(pq_row_data)
 
-        if len(pq_table_data) >= samples_per_table:
-            save_latents_table(pq_table_data, latent.shape[0], current_table_index, args.output_path, 4)
-            pq_table_data = []
-            current_table_index += 1
+            if len(pq_table_data) >= samples_per_table:
+                save_latents_table(pq_table_data, latent.shape[0], current_table_index, args.output_path, 4)
+                pq_table_data = []
+                current_table_index += 1
 
-        if (i + 1) % 100 == 0:
-            print(f'Processed {i + 1} samples')
-
-    if pq_table_data:
-        save_latents_table(pq_table_data, latent.shape[0], current_table_index, args.output_path, 4)
+    if len(pq_table_data) > 0:
+        save_latents_table(
+            pq_table_data, 
+            len(pq_table_data[0]['latents']), 
+            current_table_index, 
+            args.output_path, 
+            4
+        )
 
     print('Finished processing all samples')
 

fe81428f5cefa2ccd6de4fa7cc2dbaca917a8f8d
hayden-donnelly <hayden.git@proton.me>
2024-07-26 00:12:43 -0400

    Added bindings for opengl_proc_gen rot matrix functions.

--- submodules/opengl-proc-gen/csrc/ffi.cpp
+++ submodules/opengl-proc-gen/csrc/ffi.cpp
@@ -35,31 +35,6 @@ py::array_t<float> mat4_to_numpy(const mat4 m)
 PYBIND11_MODULE(cpp_ffi, m) 
 {
     m.def(
-        "mat4_copy", 
-        [](py::array_t<float> source) 
-        {
-            mat4 src, result;
-            numpy_to_mat4(source, src);
-            mat4_copy(src, result);
-            return mat4_to_numpy(result);
-        }
-    );
-
-    m.def(
-        "mat4_translate", 
-        [](py::array_t<float> m, py::array_t<float> t) 
-        {
-            mat4 mat, result;
-            vec3 translation;
-            numpy_to_mat4(m, mat);
-            auto r = t.unchecked<1>();
-            for (int i = 0; i < 3; i++) translation[i] = r(i);
-            mat4_translate(mat, translation, result);
-            return mat4_to_numpy(result);
-        }
-    );
-
-    m.def(
         "mat4_mul", 
         [](py::array_t<float> a, py::array_t<float> b)
         {
@@ -72,25 +47,41 @@ PYBIND11_MODULE(cpp_ffi, m)
     );
 
     m.def(
-        "mat4_scale_affine", 
-        [](py::array_t<float> m, py::array_t<float> s)
-        {
-            mat4 mat, result;
-            vec3 scale;
-            numpy_to_mat4(m, mat);
-            auto r = s.unchecked<1>();
-            for (int i = 0; i < 3; i++) scale[i] = r(i);
-            mat4_scale_affine(mat, scale, result);
-            return mat4_to_numpy(result);
-        }
-    );
-
-    m.def(
         "mat4_make_identity", 
         []() 
         {
             mat4 result;
             mat4_make_identity(result);
+            return mat4_to_numpy(result);
+        }
+    );
+
+    m.def(
+        "mat4_make_x_rotation",
+        [](float degrees)
+        {
+            mat4 result;
+            mat4_make_x_rotation(degrees, result);
+            return mat4_to_numpy(result);
+        }
+    );
+
+    m.def(
+        "mat4_make_y_rotation",
+        [](float degrees)
+        {
+            mat4 result;
+            mat4_make_y_rotation(degrees, result);
+            return mat4_to_numpy(result);
+        }
+    );
+    
+    m.def(
+        "mat4_make_y_rotation",
+        [](float degrees)
+        {
+            mat4 result;
+            mat4_make_y_rotation(degrees, result);
             return mat4_to_numpy(result);
         }
     );

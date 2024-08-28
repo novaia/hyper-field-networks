a0b98f5d2860d3066bb814ea51c24921d37d508e
hayden-donnelly <hayden.git@proton.me>
2024-08-27 22:08:38 -0400

    Added data loading for LSTM hypernetwork.

--- hypernets/lstm_hypernet.py
+++ hypernets/lstm_hypernet.py
@@ -1,3 +1,5 @@
+import os, json
+import datasets
 import jax
 import jax.numpy as jnp
 from jax import random
@@ -8,6 +10,33 @@ import matplotlib.pyplot as plt
 import optax
 from functools import partial
 from typing import Any
+
+def load_dataset(dataset_path, test_size, split_seed):
+    field_config = None
+    with open(os.path.join(dataset_path, 'field_config.json'), 'r') as f:
+        field_config = json.load(f)
+    assert field_config is not None
+    param_map = None
+    with open(os.path.join(dataset_path, 'param_map.json'), 'r') as f:
+        param_map = json.load(f)
+    assert param_map is not None
+    
+    parquet_dir = os.path.join(dataset_path, 'data')
+    parquet_paths = [
+        os.path.join(parquet_dir, p) 
+        for p in os.listdir(parquet_dir) if p.endswith('.parquet')
+    ]
+    num_parquet_files = len(parquet_paths)
+    assert num_parquet_files > 0
+    print(f'Found {num_parquet_files} parquet file(s) in dataset directory')
+
+    dataset = datasets.load_dataset('parquet', data_files=parquet_paths)
+    train, test = dataset['train'].train_test_split(test_size=test_size, seed=split_seed).values()
+    device = str(jax.devices('gpu')[0])
+    train = train.with_format('jax', device=device)
+    test = test.with_format('jax', device=device)
+    context_length = train[0]['tokens'].shape[0]
+    return train, test, field_config, param_map, context_length
 
 class LSTM(nn.Module):
     features: int
@@ -43,10 +72,19 @@ def train_step(state, tokens, start_tokens):
     return loss, state
 
 def main():
+    output_path = 'data/ar_hypernet_output/6'
+    dataset_path = 'data/colored-monsters-ngp-image-18k-16bit'
+    split_size = 0.2
+    split_seed = 0
+    train_set, test_set, field_config, param_map, sequence_length = \
+        load_dataset(dataset_path, split_size, split_seed)
+    print('image width', field_config['image_width'])
+    print('image height', field_config['image_height'])
+    print('sequence length', sequence_length)
+
     features = 128
     vocab_size = 2**16
     batch_size = 4
-    sequence_length = 18000
     learning_rate = 3e-4
     start_token = vocab_size
     model_dtype = jnp.bfloat16

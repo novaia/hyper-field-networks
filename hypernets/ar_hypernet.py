4ca9b54d754a1a90f5a8e533be0f5b33f54d0af2
hayden-donnelly <hayden.git@proton.me>
2024-09-05 15:28:55 -0400

    Adjusted ar_hypernet hyperparams.
    Increased hidden dim and num heads. Decreased num blocks to accomodate
    increased hidden dim.

--- hypernets/ar_hypernet.py
+++ hypernets/ar_hypernet.py
@@ -137,7 +137,7 @@ def sample_context(state, prompt_tokens, vocab_size, context_length, temperature
     return tokens
 
 def main():
-    output_path = 'data/ar_hypernet_output/13'
+    output_path = 'data/ar_hypernet_output/14'
     dataset_path = 'data/colored-primitives-ngp-image-2291-16bit'
     split_size = 0.2
     split_seed = 0
@@ -155,10 +155,10 @@ def main():
 
     num_epochs = 200
     batch_size = 4
-    hidden_dim = 128
-    ff_dim = 128
-    num_attention_heads = 16
-    num_blocks = 24
+    hidden_dim = 256
+    ff_dim = 256
+    num_attention_heads = 32
+    num_blocks = 10
     learning_rate = 1e-4
     weight_decay = 1e-6
     sample_temperature = 1.0

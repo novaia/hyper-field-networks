115f212c0a90c8fb490852f8a0d8acbf87f60e1a
hayden-donnelly <hayden.git@proton.me>
2024-09-09 20:39:49 -0400

    Increased ar_hypernet num_blocks to 24.
    The model can still be trained at this depth on a 3070 if the JAX memory
    pre-alloc variable is set high enough, e.g.
    
    XLA_PYTHON_CLIENT_MEM_FRACTION=.99 python -m hypernets.ar_hypernet

--- hypernets/ar_hypernet.py
+++ hypernets/ar_hypernet.py
@@ -137,7 +137,7 @@ def sample_context(state, prompt_tokens, vocab_size, context_length, temperature
     return tokens
 
 def main():
-    output_path = 'data/ar_hypernet_output/14'
+    output_path = 'data/ar_hypernet_output/15'
     dataset_path = 'data/colored-primitives-ngp-image-2291-16bit'
     split_size = 0.2
     split_seed = 0
@@ -158,7 +158,7 @@ def main():
     hidden_dim = 256
     ff_dim = 256
     num_attention_heads = 32
-    num_blocks = 10
+    num_blocks = 24
     learning_rate = 1e-4
     weight_decay = 1e-6
     sample_temperature = 1.0

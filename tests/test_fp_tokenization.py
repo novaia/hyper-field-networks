02818944b9865fd0964ce7a6a218240ea70bed26
hayden-donnelly <hayden.git@proton.me>
2024-09-20 21:48:32 -0400

    Added fp32 to bitfield16 to fp32 pytest.
    Currently failing because of precision issues. There's probably
    something wrong with one of the kernels since fp16s should be
    reconstructed losslessly from bitfields, and if the fp16 reconstruction
    is lossless then the reconstruction error shouldn't be any greater than
    the regular tokenization kernels.

--- tests/test_fp_tokenization.py
+++ tests/test_fp_tokenization.py
@@ -14,6 +14,16 @@ def test_tokenization():
     print(detokenized_batch)
     assert jnp.allclose(initial_batch, detokenized_batch, atol=1e-3)
 
+def test_bitfield():
+    initial_batch = jax.random.normal(key=jax.random.PRNGKey(0), shape=(256,))
+    bitfields = fpt.fp32_to_bitfield16(initial_batch)
+    recovered_batch = fpt.bitfield16_to_fp32(bitfields)
+    print(initial_batch)
+    print(bitfields)
+    print(recovered_batch)
+    print(jnp.max(initial_batch - recovered_batch))
+    assert jnp.allclose(initial_batch, recovered_batch, atol=1e-3)
+
 def test_vocab_size():
     expected_vocab_size = 2**16
     computed_vocab_size = fpt.get_vocab_size()

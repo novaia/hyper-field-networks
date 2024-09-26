160d24b9f2d4351c6d952fc2138e955a9b684982
hayden-donnelly <hayden.git@proton.me>
2024-09-26 17:50:31 -0400

    Modified check_detokenized_field to support u8 tokenization.

--- fields/util_scripts/check_detokenized_field.py
+++ fields/util_scripts/check_detokenized_field.py
@@ -25,15 +25,23 @@ def main():
     parser.add_argument('--config', type=str, required=True)
     parser.add_argument('--image_height', type=int, required=True)
     parser.add_argument('--image_width', type=int, required=True)
+    parser.add_argument('--u8', action='store_true', default=False)
     args = parser.parse_args()
     
+    if args.u8:
+        tokenize_fn = fpt.u8_tokenize
+        detokenize_fn = fpt.u8_detokenize
+    else:
+        tokenize_fn = fpt.tokenize
+        detokenize_fn = fpt.detokenize
+
     with open(args.config, 'r') as f:
         config = json.load(f)
     assert(config is not None)
     print(config)
 
     params_cpu = dict(jnp.load(args.field, allow_pickle=True).tolist())
-    print(params_cpu)
+    #print(params_cpu)
     model = create_model_from_config(config)
     state = create_train_state(model, learning_rate=1e-3, KEY=jax.random.PRNGKey(0))
     render_fn = functools.partial(
@@ -51,8 +59,10 @@ def main():
     # Render field with detokenized params.
     param_map, num_params = generate_param_map(module=initial_params)
     flattened_params = flatten_params(module=initial_params, param_map=param_map, num_params=num_params)
+    tokenized_params = tokenize_fn(flattened_params)
+    print(tokenized_params)
     detokenized_params = unflatten_params(
-        flat_params=fpt.detokenize(fpt.tokenize(flattened_params)), 
+        flat_params=detokenize_fn(tokenized_params), 
         param_map=param_map
     )
     state = state.replace(params=detokenized_params)

13c37e2d67e76fe0e0d74aafc57eab159024bd2d
hayden-donnelly <hayden.git@proton.me>
2024-09-30 14:23:22 -0400

    Added script to turn field dataset into bf16 byte pair tokens.

---
+++ hypernets/util_scripts/byte_pair_tokenize_field_dataset.py
@@ -0,0 +1,105 @@
+import os, argparse, json
+import jax
+import numpy as np
+import datasets
+import pyarrow as pa
+import pyarrow.parquet as pq
+import fp_tokenization as fpt
+
+# python -m hypernets.util_scripts.byte_pair_tokenize_field_dataset --dataset data/colored-primitives-ngp-image-2291 --out data/colored-primitives-ngp-image-2291-byte-pair
+
+def load_dataset(dataset_path):
+    field_config = None
+    with open(os.path.join(dataset_path, 'field_config.json'), 'r') as f:
+        field_config = json.load(f)
+    assert field_config is not None
+    param_map = None
+    with open(os.path.join(dataset_path, 'param_map.json'), 'r') as f:
+        param_map = json.load(f)
+    assert param_map is not None
+    
+    parquet_dir = os.path.join(dataset_path, 'data')
+    parquet_paths = [
+        os.path.join(parquet_dir, p) 
+        for p in os.listdir(parquet_dir) if p.endswith('.parquet')
+    ]
+    num_parquet_files = len(parquet_paths)
+    assert num_parquet_files > 0
+    print(f'Found {num_parquet_files} parquet file(s) in dataset directory')
+
+    dataset = datasets.load_dataset('parquet', data_files=parquet_paths)
+    full_dataset = dataset['train']  # Use the entire dataset without splitting
+
+    device = str(jax.devices('gpu')[0])
+    full_dataset = full_dataset.with_format('jax', device=device)
+    context_length = full_dataset[0]['params'].shape[0]
+    return full_dataset, field_config, param_map, context_length
+
+def save_table(table_data, context_length, table_number, output_path, zfill_amount):
+    print(f'Entries in table {table_number}: {len(table_data)}')
+    schema = pa.schema(
+        fields=[
+            ('tokens', pa.list_(pa.uint8(), list_size=context_length)),
+        ],
+        metadata={
+            b'huggingface': json.dumps({
+                'info': {
+                    'features': {
+                        'tokens': {'_type': 'Sequence', 'feature': {'_type': 'Value', 'dtype': 'uint8'}},
+                    }
+                }
+            }).encode('utf-8')
+        }
+    )
+    table = pa.Table.from_pylist(table_data, schema=schema)
+    pq.write_table(table, f'{output_path}/{str(table_number).zfill(zfill_amount)}.parquet')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--out', type=str, required=True)
+    parser.add_argument('--samples_per_table', type=int, default=16384)
+    args = parser.parse_args()
+
+    if not os.path.exists(args.out):
+        os.makedirs(args.out)
+    out_data_path = os.path.join(args.out, 'data')
+    if not os.path.exists(out_data_path):
+        os.makedirs(out_data_path)
+
+    dataset, field_config, param_map, context_length = load_dataset(args.dataset)
+    context_length_out = context_length * 2
+
+    with open(os.path.join(args.out, 'field_config.json'), 'w') as f:
+        json.dump(obj=field_config, fp=f)
+
+    with open(os.path.join(args.out, 'param_map.json'), 'w') as f:
+        json.dump(obj=param_map, fp=f)
+    
+    num_samples = len(dataset)
+    samples_per_table = args.samples_per_table
+    pq_table_data = []
+    samples_in_current_table = 0
+    current_table_index = 0
+    for i in range(num_samples):
+        tokens = fpt.byte_pair_tokenize(dataset[i]['params'])
+        pq_row_data = {
+            'tokens': np.array(tokens, dtype=tokens.dtype).tolist(),
+        }
+        pq_table_data.append(pq_row_data)
+        samples_in_current_table += 1
+        if samples_in_current_table >= samples_per_table:
+            save_table(
+                pq_table_data, context_length_out, current_table_index, out_data_path, 4
+            )
+            pq_table_data = []
+            samples_in_current_table = 0
+            current_table_index += 1
+    if samples_in_current_table > 0:
+        save_table(
+            pq_table_data, context_length_out, current_table_index, out_data_path, 4
+        )
+
+if __name__ == '__main__':
+    main()

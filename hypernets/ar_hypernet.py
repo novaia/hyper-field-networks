c0737e5282d2d3935913e52f56f79f8504c249b6
hayden-donnelly <hayden.git@proton.me>
2024-09-30 15:39:43 -0400

    Revert to single byte ar hypernet.

--- hypernets/ar_hypernet.py
+++ hypernets/ar_hypernet.py
@@ -108,12 +108,12 @@ def train_step(state, tokens, start_tokens):
 def test_step(state, tokens, start_tokens):
     input_tokens = jnp.concatenate([start_tokens, tokens[..., :-1]], axis=-1)
     target_tokens = tokens
-    logits = state.apply_fn({'params': state.params}, tokens=input_tokens, training=False)
+    logits = jax.lax.stop_gradient(state.apply_fn({'params': state.params}, tokens=input_tokens, training=False))
     loss = jnp.mean(optax.softmax_cross_entropy_with_integer_labels(logits, target_tokens))
     return loss
 
 def sample_context(state, prompt_tokens, vocab_size, context_length, temperature=1.0):
-    tokens = jnp.zeros((1, context_length), dtype=jnp.uint32)
+    tokens = jnp.zeros((1, context_length), dtype=jnp.uint8)
     for i in range(len(prompt_tokens)):
         tokens = tokens.at[0, i].set(prompt_tokens[i])
     vocab = jnp.arange(vocab_size, dtype=jnp.uint32)
@@ -139,15 +139,9 @@ def sample_context(state, prompt_tokens, vocab_size, context_length, temperature
     
     return tokens
 
-def preprocess_tokens(tokens: jax.Array):
-    tokens_reshape = jnp.reshape(tokens, (tokens.shape[0], tokens.shape[1]//2, 2))
-    mantissa_tokens = tokens_reshape[:, :, 0]
-    exponent_tokens = tokens_reshape[:, :, 1]
-    return mantissa_tokens, exponent_tokens
-
 def main():
+    u8_tokenization = True
     output_path = 'data/ar_hypernet_output/17'
-    #dataset_path = 'data/colored-primitives-ngp-image-2291-byte-pair'
     dataset_path = 'data/colored-primitives-ngp-image-2291-8bit'
     split_size = 0.2
     split_seed = 0
@@ -160,62 +154,26 @@ def main():
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    vocab_size = 2**8
-    #tokenize_fn = fpt.byte_pair_tokenize
-    #detokenize_fn = fpt.byte_pair_detokenize
-    tokenize_fn = fpt.u8_tokenize
-    detokenize_fn = fpt.u8_detokenize
-
-    field_model = ngp_image.create_model_from_config(field_config)
-    field_state = ngp_image.create_train_state(field_model, 3e-4, jax.random.PRNGKey(0))
-    tokens = jnp.array(jnp.expand_dims(train_set[200]['tokens'], axis=0), dtype=jnp.uint8)
-    flat_params = detokenize_fn(jnp.ravel(tokens))
-    print(flat_params)
-    params = unflatten_params(jnp.array(flat_params, dtype=jnp.float32), param_map)
-    field_state = field_state.replace(params=params)
-    field_render = ngp_image.render_image(
-        field_state, field_config['image_height'], field_config['image_width'], field_config['channels']
-    )
-    field_render = jax.device_put(field_render, jax.devices('cpu')[0])
-    plt.imsave('data/fp8_quality_check.png', field_render)
-    exit()
-
-    '''
-    field_model = ngp_image.create_model_from_config(field_config)
-    field_state = ngp_image.create_train_state(field_model, 3e-4, jax.random.PRNGKey(0))
-    
-    # For some reason HF Datasets doesn't store the tokens as uint8, so they need to be converted.
-    tokens = jnp.array(jnp.expand_dims(train_set[300]['tokens'], axis=0), dtype=jnp.uint8)
-    #print(tokens[0, :32])
-    #mantissa_tokens, exponent_tokens = preprocess_tokens(tokens)
-    #print(mantissa_tokens[0, :16])
-    #print(exponent_tokens[0, :16])
-    flat_params = detokenize_fn(jnp.ravel(tokens))
-    #print(flat_params)
-    #print(flat_params.shape)
-    #exit()
-    params = unflatten_params(jnp.array(flat_params, dtype=jnp.float32), param_map)
-    #print(params)
-    #print(field_config)
-    field_state = field_state.replace(params=params)
-    field_render = ngp_image.render_image(
-        field_state, field_config['image_height'], field_config['image_width'], field_config['channels']
-    )
-    field_render = jax.device_put(field_render, jax.devices('cpu')[0])
-    plt.imsave('data/byte_pair_quality_check.png', field_render)
-    exit()
-    '''
+    if u8_tokenization:
+        vocab_size = fpt.get_u8_vocab_size()
+        tokenize_fn = fpt.u8_tokenize
+        detokenize_fn = fpt.u8_detokenize
+    else:
+        vocab_size = fpt.get_vocab_size()
+        tokenize_fn = fpt.tokenize
+        detokenize_fn = fpt.detokenize
+    print('Vocab size', vocab_size)
 
     num_epochs = 200
-    batch_size = 4
-    hidden_dim = 256
-    ff_dim = 256
-    num_attention_heads = 32
-    num_blocks = 24
+    batch_size = 8
+    hidden_dim = 64
+    ff_dim = 128
+    num_attention_heads = 16
+    num_blocks = 12
     learning_rate = 1e-4
     weight_decay = 1e-6
     sample_temperature = 1.0
-    dropout_rate = 0.2
+    dropout_rate = 0.05
     start_token = vocab_size
     batched_start_tokens = jnp.full((batch_size, 1), fill_value=start_token, dtype=jnp.uint8)
 
@@ -242,15 +200,18 @@ def main():
         dropout_rate=dropout_rate,
         ff_dim=ff_dim
     )
-    x = jnp.ones((batch_size, context_length), dtype=jnp.uint32)
+    x = jnp.ones((batch_size, context_length), dtype=jnp.uint8)
     params_key = jax.random.PRNGKey(91)
-    params = model.init(params_key, tokens=x, training=False)['params']
+    params = jax.jit(partial(model.init, training=False))(params_key, tokens=x)['params']
     opt = optax.chain(
         optax.zero_nans(),
         optax.adamw(learning_rate=learning_rate, weight_decay=weight_decay)
     )
     state = TrainState.create(apply_fn=model.apply, params=params, tx=opt)
     
+    field_model = ngp_image.create_model_from_config(field_config)
+    field_state = ngp_image.create_train_state(field_model, 3e-4, jax.random.PRNGKey(0))
+
     num_train_samples = len(train_set)
     train_steps = num_train_samples // batch_size
     print('Train set size:', num_train_samples)
@@ -269,7 +230,7 @@ def main():
         train_iterator = train_set.iter(batch_size)
         accumulated_losses = []
         for step in range(train_steps):
-            tokens = next(train_iterator)['tokens']
+            tokens = jnp.array(next(train_iterator)['tokens'], dtype=jnp.uint8)
             loss, state = train_step(state, tokens=tokens, start_tokens=batched_start_tokens)
             accumulated_losses.append(loss)
             steps_since_loss_report += 1
@@ -284,12 +245,12 @@ def main():
         test_iterator = test_set.iter(batch_size)
         losses_this_test = []
         for step in range(test_steps):
-            tokens = next(test_iterator)['tokens']
+            tokens = jnp.array(next(test_iterator)['tokens'], dtype=jnp.uint8)
             loss = test_step(state, tokens=tokens, start_tokens=batched_start_tokens)
             losses_this_test.append(loss)
         average_test_loss = sum(losses_this_test) / len(losses_this_test)
         wandb.log({'test_loss': average_test_loss}, step=state.step)
-        #print(f'epoch {epoch}, test_loss {average_test_loss}')
+        print(f'epoch {epoch}, test_loss {average_test_loss}')
         
         tokens = sample_context(
             state, 
@@ -298,7 +259,7 @@ def main():
             context_length=context_length, 
             temperature=sample_temperature
         )[0]
-        print(tokens)
+        tokens = jnp.array(tokens, dtype=jnp.uint8)
         flat_params = detokenize_fn(tokens)
         flat_params = jnp.nan_to_num(flat_params)
         params = unflatten_params(jnp.array(flat_params, dtype=jnp.float32), param_map)
